ffb0b36214ae69943f985b33d833c2217ab3457d
jahnestacado <ioannis.kiwe@gmail.com>
2013-01-08 15:15:44 +0100

    clean project

---
+++ jahnestacado/QLJava/src/org/uva/sea/ql/ast/Or.java
@@ -0,0 +1,15 @@
+package org.uva.sea.ql.ast;
+
+import org.uva.sea.ql.ast.Expr;
+
+public class Or extends Expr {
+
+	public Or() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Or(Expr result, Expr rhs) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
