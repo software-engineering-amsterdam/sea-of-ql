14fa53e01d5f8083502721191d129497d05a00df
Alberto <gonzovilla89@gmail.com>
2013-01-08 16:25:10 +0100

    one parser error free

--- gonzovilla/src/org/uva/sea/ql/parser/test/TestExpressions.java
+++ gonzovilla/src/org/uva/sea/ql/parser/test/TestExpressions.java
@@ -2,7 +2,7 @@ package org.uva.sea.ql.parser.test;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -17,8 +17,6 @@ import org.uva.sea.ql.ast.LEq;
 import org.uva.sea.ql.ast.LT;
 import org.uva.sea.ql.ast.Mul;
 import org.uva.sea.ql.parser.antlr.ANTLRParser;
-import org.uva.sea.ql.parser.jacc.JACCParser;
-import org.uva.sea.ql.parser.rats.RatsParser;
 
 @RunWith(Parameterized.class)
 public class TestExpressions {
@@ -27,11 +25,10 @@ public class TestExpressions {
 
 	@Parameters
 	public static List<Object[]> theParsers() {
-	  return Arrays.asList(
-			  new Object[] {new JACCParser()}, 
-			  new Object[] {new RatsParser()},
-			  new Object[] {new ANTLRParser()}
-			 );
+		List<Object[]> parserList = new ArrayList<Object[]>();
+		parserList.add(new Object[] {new ANTLRParser()});
+		
+		return parserList;
 	}
 
 	
