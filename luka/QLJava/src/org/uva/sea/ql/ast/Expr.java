10d2a440b6212101a9fa242c7db612193370c3c5
mrluka <luc0@hotmail.de>
2013-01-18 20:20:54 +0100

    delete

--- luka/QLJava/src/org/uva/sea/ql/ast/Expr.java
+++
@@ -1,6 +0,0 @@
-package org.uva.sea.ql.ast;
-
-public abstract class Expr implements ASTNode {
-
-	//public abstract void printExprType();
-}
--- luka/QLJava/src/org/uva/sea/ql/ast/Neg.java
+++
@@ -1,8 +0,0 @@
-package org.uva.sea.ql.ast;
-
-public class Neg extends Expr{
-
-	public Neg(Object x){
-		
-	}
-}
