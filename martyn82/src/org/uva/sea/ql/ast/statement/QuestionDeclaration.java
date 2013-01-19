a31cc5300678a266f35d6bad540668d3e4040390
Martijn Endenburg <martijn.endenburg@gmail.com>
2013-01-19 12:06:11 +0100

    Added form and question declaration statements.

---
+++ martyn82/src/org/uva/sea/ql/ast/statement/QuestionDeclaration.java
@@ -0,0 +1,64 @@
+package org.uva.sea.ql.ast.statement;
+
+import org.uva.sea.ql.ast.expression.value.Str;
+import org.uva.sea.ql.visitor.INodeVisitor;
+
+/**
+ * Represents a question declaration.
+ */
+public class QuestionDeclaration extends Statement {
+	/**
+	 * Holds the name node.
+	 */
+	private final Str name;
+	
+	/**
+	 * Holds the declaration node.
+	 */
+	private final Statement statement;
+	
+	/**
+	 * Constructs a new question declaration node.
+	 * 
+	 * @param name
+	 * @param declaration
+	 */
+	public QuestionDeclaration( Str name, VarDeclaration declaration ) {
+		this.name = name;
+		this.statement = declaration;
+	}
+	
+	/**
+	 * Constructs a new question declaration.
+	 * 
+	 * @param name
+	 * @param assignment
+	 */
+	public QuestionDeclaration( Str name, Assignment assignment ) {
+		this.name = name;
+		this.statement = assignment;
+	}
+	
+	/**
+	 * Retrieves the name node.
+	 * 
+	 * @return Name AST node.
+	 */
+	public Str getName() {
+		return this.name;
+	}
+	
+	/**
+	 * Retrieves the declaration node.
+	 * 
+	 * @return Declaration AST node.
+	 */
+	public Statement getDeclaration() {
+		return this.statement;
+	}
+
+	@Override
+	public void accept( INodeVisitor visitor ) {
+		visitor.visit( this );
+	}
+}
