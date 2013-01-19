6d2c4356b98b34ae0668e0939c5b723ad62c05b9
Martijn Endenburg <martijn.endenburg@gmail.com>
2013-01-19 15:16:48 +0100

    No more generalized toString for unaries.

--- martyn82/src/org/uva/sea/ql/ast/expression/UnaryExpression.java
+++ martyn82/src/org/uva/sea/ql/ast/expression/UnaryExpression.java
@@ -10,32 +10,27 @@ abstract public class UnaryExpression extends Expression {
 	 * Holds the expression.
 	 */
 	private final Expression expression;
-	
+
 	/**
 	 * Constructs a new UnaryExpression.
-	 * 
+	 *
 	 * @param expression
 	 */
 	protected UnaryExpression( Expression expression ) {
 		this.expression = expression;
 	}
-	
+
 	/**
 	 * Retrieves the expression.
-	 * 
+	 *
 	 * @return The expression.
 	 */
 	public Expression getExpression() {
 		return this.expression;
 	}
-	
+
 	@Override
 	public void accept( INodeVisitor visitor ) {
 		visitor.visit( this );
-	}
-	
-	@Override
-	public String toString() {
-		return this.getClass().getSimpleName();
 	}
 }
