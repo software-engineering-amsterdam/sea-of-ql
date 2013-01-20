4341109458468092bfb795ccecda9e37fafd8255
Thanasis <thanasis.papapanagiotou@emesa.nl>
2013-01-20 14:14:52 +0100

    thanpa: Creation of needed classes

---
+++ thanpa/QLJava/src/org/uva/sea/ql/parser/antlr/NEq.java
@@ -0,0 +1,11 @@
+package org.uva.sea.ql.parser.antlr;
+
+import org.uva.sea.ql.ast.Expr;
+
+public class NEq extends Expr {
+
+	public NEq(Expr result, Expr rhs) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
