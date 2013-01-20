a67295aa9c695e278e446796b5ec1ff08d23cbe9
mrluka <luc0@hotmail.de>
2013-01-20 18:26:43 +0100

    Removed error

--- luka/src/org/uva/sea/ql/parser/antlr/__Test__.java
+++ luka/src/org/uva/sea/ql/parser/antlr/__Test__.java
@@ -8,7 +8,7 @@ public class __Test__ {
         QLLexer lex = new QLLexer(new ANTLRFileStream("/Users/luc0/Desktop/Software_Creation/code/QLJava/src/org/uva/sea/ql/parser/antlr/__Test___input.txt", "UTF8"));
         CommonTokenStream tokens = new CommonTokenStream(lex);
 
-        QLParser g = new QLParser(tokens,49100,null);
+        QLParser g = new QLParser(tokens); //,49100,null
         try {
            g.parse();
         } catch (RecognitionException e) {
