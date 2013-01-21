f364c9524a3b8a501627769de02192314d623f70
caytekin <cigdemaytekin_872@hotmail.com>
2013-01-21 14:34:34 +0100

    question and computedQuestion are working

--- caytekin/QLJava/src/org/uva/sea/ql/parser/antlr/ANTLRComputedQuestionParser.java
+++ caytekin/QLJava/src/org/uva/sea/ql/parser/antlr/ANTLRComputedQuestionParser.java
@@ -4,12 +4,12 @@ import org.antlr.runtime.RecognitionException;
 import org.uva.sea.ql.ast.ASTNode;
 import org.uva.sea.ql.parser.test.ParseError;
 
-public class ANTLRExpressionParser extends GeneralANTLRParser{
+public class ANTLRComputedQuestionParser extends GeneralANTLRParser{
 	
-	public ASTNode parseANonTerminal (QLParser parser) throws ParseError {
-
+	public ASTNode parseANonTerminal(QLParser parser) throws ParseError {
 		try {
-			return parser.orExpr();
+			System.out.println("Testing computed questions...");
+			return parser.computedQuestion();
 		} catch (RecognitionException e) {
 			throw new ParseError(e.getMessage());
 		}
