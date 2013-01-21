936f9d7b8dfaa94d074f8a4639686a4c38b81516
jahnestacado <ioannis.kiwe@gmail.com>
2013-01-21 15:13:19 +0100

    all form tests succeed

--- jahnestacado/QLJava/src/org/uva/sea/ql/parser/antlr/QLParser.java
+++ jahnestacado/QLJava/src/org/uva/sea/ql/parser/antlr/QLParser.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.4 /home/jahn/workspace1/jahnestacado/jahnestacado/QLJava/src/org/uva/sea/ql/parser/antlr/QL.g 2013-01-21 13:46:26
+// $ANTLR 3.4 /home/jahn/workspace1/jahnestacado/jahnestacado/QLJava/src/org/uva/sea/ql/parser/antlr/QL.g 2013-01-21 14:24:38
 
 package org.uva.sea.ql.parser.antlr;
 import org.uva.sea.ql.ast.*;
