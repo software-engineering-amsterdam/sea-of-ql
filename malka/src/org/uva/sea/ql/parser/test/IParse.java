5c89812002b27243643b75144c1b3be16e0e8dbe
Malkaduhimi <Mohamed@Alkaduhimi.nl>
2013-01-21 17:14:23 +0100

    AST parsing ready for action

--- malka/src/org/uva/sea/ql/parser/test/IParse.java
+++ malka/src/org/uva/sea/ql/parser/test/IParse.java
@@ -1,12 +1,12 @@
 package org.uva.sea.ql.parser.test;
 
-import org.uva.sea.ql.ast.Expr;
+import org.uva.sea.ql.ast.Expression;
 import org.uva.sea.ql.ast.form.Form;
 import org.uva.sea.ql.ast.form.FormElement;
 import java.util.List;
 
 public interface IParse {
-	Expr parseExpr(String src) throws ParseError;
+	Expression parseExpr(String src) throws ParseError;
 	Form parseForm(String src) throws ParseError;
 	FormElement parseFormElement(String src) throws ParseError;
 	List<FormElement> parseFormElements(String src) throws ParseError;
