eebfb44142e004a0c8391db76528f3dbfd5836e4
Edwin <edwin.vanmanen@student.uva.nl>
2013-01-22 18:07:26 +0100

    Refactored ANTLR Parsers to be more generic and minify try catch duplication

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/antlr/ANTLRParserExpressions.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/antlr/ANTLRParserExpressions.java
@@ -4,15 +4,9 @@ import org.uva.sea.ql.ast.Expr;
 import org.uva.sea.ql.parser.IParser;
 import org.uva.sea.ql.parser.ParseError;
 
-import org.antlr.runtime.RecognitionException;
-
 public class ANTLRParserExpressions extends ANTLRParser implements IParser {
 	@Override
 	public Expr parse(String src) throws ParseError {
-		try {
-			return getParser(src).orExpr();
-		} catch (RecognitionException e) {
-			throw new ParseError(e.getMessage());
-		}
+		return (Expr)parseInput(src, "orExpr");
 	}
 }
--- edwinvm/QLJava/src/org/uva/sea/ql/parser/antlr/ANTLRParserTypes.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/antlr/ANTLRParserTypes.java
@@ -3,15 +3,9 @@ package org.uva.sea.ql.parser.antlr;
 import org.uva.sea.ql.ast.Type;
 import org.uva.sea.ql.parser.ParseError;
 
-import org.antlr.runtime.RecognitionException;
-
 public class ANTLRParserTypes extends ANTLRParser {
 	@Override
 	public Type parse(String src) throws ParseError {
-		try {
-			return getParser(src).type();
-		} catch (RecognitionException e) {
-			throw new ParseError(e.getMessage());
-		}
+		return (Type)parseInput(src, "type");
 	}
 }
