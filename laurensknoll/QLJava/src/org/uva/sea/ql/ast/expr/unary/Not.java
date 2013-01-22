dedc56bcb17fc460214eecd780d51ee58ae3f329
Laurens Knoll <laurensknoll@gmail.com>
2013-01-22 19:36:58 +0100

    Extended accept method to return generic result

--- laurensknoll/QLJava/src/org/uva/sea/ql/ast/expr/unary/Not.java
+++ laurensknoll/QLJava/src/org/uva/sea/ql/ast/expr/unary/Not.java
@@ -10,8 +10,8 @@ public class Not extends AbstractUnary {
 	}
 
 	@Override
-	public void accept(Expression<?> visitor) {
-		visitor.visit(this);
+	public <T> T accept(Expression<T> visitor) {
+		return visitor.visit(this);
 	}
 
 }
