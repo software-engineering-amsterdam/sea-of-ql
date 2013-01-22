745c4245cf9af8b0594533ad10b319fae7e0f34c
mrluka <luc0@hotmail.de>
2013-01-22 08:43:08 +0100

    Str

---
+++ luka/src/org/uva/sea/ql/ast/type/Str.java
@@ -0,0 +1,11 @@
+package org.uva.sea.ql.ast.type;
+
+public class Str extends Type{
+
+	@Override
+	public boolean isCompatibleTo(Type t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
