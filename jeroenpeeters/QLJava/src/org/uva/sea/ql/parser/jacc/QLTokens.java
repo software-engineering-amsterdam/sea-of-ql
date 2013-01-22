08f4a2463210ba476140c93d5f8b706d4ef36640
jeroenpeeters <jeroen@peetersweb.nl>
2013-01-22 10:34:38 +0100

    new JaccEcliplse plugin relase added IfElseStatement

--- jeroenpeeters/QLJava/src/org/uva/sea/ql/parser/jacc/QLTokens.java
+++ jeroenpeeters/QLJava/src/org/uva/sea/ql/parser/jacc/QLTokens.java
@@ -1,4 +1,4 @@
-// Output created by jacc on Sun Jan 20 21:30:03 CET 2013
+// Output created by jacc on Tue Jan 22 09:58:50 CET 2013
 
 package org.uva.sea.ql.parser.jacc;
 
@@ -10,20 +10,21 @@ interface QLTokens {
     int DT_INTEGER = 4;
     int DT_MONEY = 5;
     int DT_TEXT = 6;
-    int EQ = 7;
-    int FORM = 8;
-    int GEQ = 9;
-    int IDENT = 10;
-    int IF = 11;
-    int INT = 12;
-    int LEQ = 13;
-    int NEQ = 14;
-    int OR = 15;
-    int TEXT = 16;
-    int UMIN = 17;
-    int UNOT = 18;
-    int UPLUS = 19;
-    int error = 20;
+    int ELSE = 7;
+    int EQ = 8;
+    int FORM = 9;
+    int GEQ = 10;
+    int IDENT = 11;
+    int IF = 12;
+    int INT = 13;
+    int LEQ = 14;
+    int NEQ = 15;
+    int OR = 16;
+    int TEXT = 17;
+    int UMIN = 18;
+    int UNOT = 19;
+    int UPLUS = 20;
+    int error = 21;
     // '!' (code=33)
     // '"' (code=34)
     // '(' (code=40)
