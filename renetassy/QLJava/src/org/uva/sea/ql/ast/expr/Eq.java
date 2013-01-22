2a253526b8b45e7a7a14abe68ff6c577f5931bd7
renetassy <tassyrene@gmail.com>
2013-01-22 16:38:53 +0100

    rene tassy: completed AST class  hierachy. grammar tests succeeded

---
+++ renetassy/QLJava/src/org/uva/sea/ql/ast/expr/Eq.java
@@ -0,0 +1,10 @@
+package org.uva.sea.ql.ast.expr;
+
+public class Eq extends Binary {
+
+	public Eq(Expr result, Expr rhs) {
+		super(result, rhs);
+	}
+	
+
+}
---
+++ renetassy/QLJava/src/org/uva/sea/ql/ast/expr/NEq.java
@@ -0,0 +1,11 @@
+package org.uva.sea.ql.ast.expr;
+
+
+
+public class NEq extends Binary {
+
+	public NEq(Expr result, Expr rhs) {
+		super(result, rhs);
+	}
+
+}
