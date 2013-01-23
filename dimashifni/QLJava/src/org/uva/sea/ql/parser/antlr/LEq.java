fe0188fde3e2e9035d3be3df3dab06a2d6e6f5d5
dimashifni <dimas.hifni@gmail.com>
2013-01-23 22:40:37 +0100

    Commit: Fix IDE

---
+++ dimashifni/QLJava/src/org/uva/sea/ql/parser/antlr/LEq.java
@@ -0,0 +1,12 @@
+package org.uva.sea.ql.parser.antlr;
+
+import org.uva.sea.ql.ast.Expr;
+
+public class LEq extends Expr {
+	
+	public LEq(Expr result, Expr rhs)
+	{
+		
+	}
+
+}
---
+++ dimashifni/QLJava/src/org/uva/sea/ql/parser/antlr/LT.java
@@ -0,0 +1,12 @@
+package org.uva.sea.ql.parser.antlr;
+
+import org.uva.sea.ql.ast.Expr;
+
+public class LT extends Expr {
+	
+	public LT(Expr result, Expr rhs)
+	{
+		
+	}
+
+}
