0c611088ad936478f6cb27e881c55fb8a0d58d1c
Alberto <gonzovilla89@gmail.com>
2013-01-25 17:05:56 +0100

    fixed expressions

--- gonzovilla/src/org/uva/sea/ql/ast/expr/Ident.java
+++ gonzovilla/src/org/uva/sea/ql/ast/expr/Ident.java
@@ -1,37 +1,3 @@
-<<<<<<< HEAD:pklijn/src/org/uva/sea/ql/ast/Ident.java
-package org.uva.sea.ql.ast;
-
-import java.util.Map;
-
-import org.uva.sea.ql.ast.types.Type;
-import org.uva.sea.ql.ast.values.Value;
-
-public class Ident extends Expr {
-
-	private final String name;
-
-	public Ident(String name) {
-		this.name = name;
-	}
-	
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public Value eval() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Type typeOf(Map<Ident, Type> typeEnv) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-}
-=======
 package org.uva.sea.ql.ast.expr;
 
 import java.util.Map;
@@ -58,7 +24,4 @@ public class Ident extends Expr {
 			}
 			return new TypeError();
 	}
-
-
 }
->>>>>>> 483057f28359ecac2b999d94a928c0aaf01d2a6e:gonzovilla/src/org/uva/sea/ql/ast/expr/Ident.java
