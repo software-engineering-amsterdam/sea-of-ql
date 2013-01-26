f56553c4ed3627a5dd0ce6991bbb23ee8e170ed8
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2013-01-26 22:37:14 +0100

    Update of tests

--- ruudvanderweijde/src/org/uva/sea/ql/parser/test/AllTests.java
+++ ruudvanderweijde/src/org/uva/sea/ql/parser/test/AllTests.java
@@ -6,10 +6,13 @@ import org.junit.runners.Suite.SuiteClasses;
 import org.uva.sea.ql.parser.test.tests.TestExprSemanticsBinary;
 import org.uva.sea.ql.parser.test.tests.TestExprSemanticsPrimary;
 import org.uva.sea.ql.parser.test.tests.TestExprSemanticsUnary;
+import org.uva.sea.ql.parser.test.tests.TestFormSemantics;
+import org.uva.sea.ql.parser.test.tests.TestParser;
 
 @RunWith(Suite.class)
 @SuiteClasses({ TestParser.class, TestExprSemanticsBinary.class,
-		TestExprSemanticsPrimary.class, TestExprSemanticsUnary.class })
+		TestExprSemanticsPrimary.class, TestExprSemanticsUnary.class, 
+		TestFormSemantics.class })
 public class AllTests {
 
 }
