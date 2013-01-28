efdd3fb1dc91b9a291ef104e48848a9ad6e9b506
Yosuf <yosuf.haydary@gmail.com>
2013-01-28 13:45:03 +0100

    Grammar mod, Refactoring, test

--- yosuf/test/org/uva/sea/ql/parser/test/FormTest.java
+++ yosuf/test/org/uva/sea/ql/parser/test/FormTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 import org.uva.sea.ql.ast.Form;
+import org.uva.sea.ql.ast.Question;
 import org.uva.sea.ql.parser.IParse;
 import org.uva.sea.ql.parser.ParseError;
 import org.uva.sea.ql.parser.jacc.JACCParser;
@@ -22,7 +23,7 @@ public class FormTest {
 		assertEquals(
 				Form.class,
 				parser.parse(
-						"form someForm { Boolean question1 = \"Are you married?\" }")
+						"form someForm { Boolean question1 = \"Are you married?\" ; }")
 						.getClass());
 	}
 
@@ -33,5 +34,11 @@ public class FormTest {
 				parser.parse(
 						"form someForm { Boolean question1 = \"Are you married? }")
 						.getClass());
+	}
+
+	@Test
+	public void testQuestion() throws ParseError {
+		assertEquals(Question.class,
+				parser.parse("Boolean q2 = \"How is life?\"; ").getClass());
 	}
 }
