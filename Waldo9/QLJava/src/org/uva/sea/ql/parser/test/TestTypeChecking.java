df252687e4a6479a8a6618c226e3d64735b09753
Waldo9 <contact@wouterkwakernaak.eu>
2013-01-29 13:49:34 +0100

    Updated typechecking test

--- Waldo9/QLJava/src/org/uva/sea/ql/parser/test/TestTypeChecking.java
+++ Waldo9/QLJava/src/org/uva/sea/ql/parser/test/TestTypeChecking.java
@@ -1,6 +1,9 @@
 package org.uva.sea.ql.parser.test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -21,8 +24,10 @@ public class TestTypeChecking {
 		QLFileReader qlFileReader = new QLFileReader();
 		StatementTypeCheckingVisitor typeChecker = new StatementTypeCheckingVisitor();
 		parser.parseForm(qlFileReader.readQLFile("QLTestcase2.ql")).getBlock().accept(typeChecker);
+		List<String> messages = typeChecker.getErrorMessages();		
 		for (String errorMessage : typeChecker.getErrorMessages())
 			System.out.println(errorMessage);
+		assertEquals(3, messages.size());
 	}
 
 }
