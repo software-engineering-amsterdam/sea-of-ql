b421c151e433d7a7aadd28ab72c77343686a7947
Jesse van Assen <jesse.v.assen@gmail.com>
2013-01-30 12:48:45 +0100

    Replaced the HashMap in the InterpreterVisitor with a LinkedHashMap to retain the order in which the values are inserted.

--- JessevanAssen/src/org/uva/sea/ql/interpreter/InterpreterVisitor.java
+++ JessevanAssen/src/org/uva/sea/ql/interpreter/InterpreterVisitor.java
@@ -7,10 +7,7 @@ import org.uva.sea.ql.ast.expr.*;
 import org.uva.sea.ql.ast.expr.value.*;
 import org.uva.sea.ql.interpreter.valueParser.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class InterpreterVisitor implements ASTNodeVisitor<Value, InterpreterVisitor.Context> {
 
@@ -20,13 +17,16 @@ public class InterpreterVisitor implements ASTNodeVisitor<Value, InterpreterVisi
         private final Map<String, String> values;
 
         private Context(Map<String, String> values) {
+            // A LinkedHashMap is used here instead of a regular HashMap, because a LinkedHashMap retains
+            // the order in which the values are inserted. This gives the values a better context, because
+            // they can be retrieved in the same order as they were in the form.
+            this.identifiers = new LinkedHashMap<Ident, Value>();
             this.errors = new ArrayList<Message>();
-            this.identifiers = new HashMap<Ident, Value>();
             this.values = values;
         }
 
-        public List<Message> getErrors() { return errors; }
         public Map<Ident, Value> getIdentifiers() { return identifiers; }
+        public List<Message> getErrors() { return errors; }
         public Map<String, String> getValues() { return values; }
     }
 
