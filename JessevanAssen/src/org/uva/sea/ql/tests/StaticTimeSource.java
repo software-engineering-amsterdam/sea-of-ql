93cbf914892d1a714e9027eb8a4e46cfdc4b7171
Jesse van Assen <jesse.v.assen@gmail.com>
2013-01-30 15:40:21 +0100

    Added a TimeSource interface so the current time can be retrieved but the time can also be mocked in the tests.

---
+++ JessevanAssen/src/org/uva/sea/ql/tests/StaticTimeSource.java
@@ -0,0 +1,17 @@
+package org.uva.sea.ql.tests;
+
+import org.uva.sea.ql.TimeSource;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class StaticTimeSource implements TimeSource {
+    private Calendar calendar;
+    public Calendar getDate() { return calendar; }
+    public void setDate(Calendar calendar) { this.calendar = calendar; }
+
+    @Override
+    public Calendar getCurrentDate() {
+        return calendar;
+    }
+}
