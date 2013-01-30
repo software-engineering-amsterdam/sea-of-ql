89fe6a5c2979904a2e12cd887b81840dba033416
Jesse van Assen <jesse.v.assen@gmail.com>
2013-01-30 15:47:46 +0100

    Created a value serializer for the csv format.

---
+++ JessevanAssen/src/org/uva/sea/ql/valuePersister/ValueSerializers/CsvValueSerializer.java
@@ -0,0 +1,24 @@
+package org.uva.sea.ql.valuePersister.valueSerializers;
+
+import org.uva.sea.ql.ast.expr.Ident;
+import org.uva.sea.ql.ast.expr.value.Value;
+import org.uva.sea.ql.valuePersister.ValueSerializer;
+import org.uva.sea.ql.valuePersister.ValueSerializerVisitor;
+
+import java.util.Map;
+
+public class CsvValueSerializer implements ValueSerializer {
+    @Override
+    public String serialize(Map<Ident, Value> values) {
+        final String headers = "\"Identifier\",\"Value\"";
+
+        StringBuilder csvBuilder = new StringBuilder(headers).append(System.lineSeparator());
+        for(Map.Entry<Ident, Value> entry : values.entrySet())
+            csvBuilder
+                    .append("\"").append(entry.getKey()).append("\",")
+                    .append("\"").append(entry.getValue()).append("\"")
+                    .append(System.lineSeparator());
+
+        return csvBuilder.toString();
+    }
+}
