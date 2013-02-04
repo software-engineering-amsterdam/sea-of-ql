a738a4d1fda85ff6481ab216e3aa448d2e127978
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-04 17:49:11 +0100

    Refactored CheckExpr visitor to reflect changes in relational and logical expr checking

--- edwinvm/QLJava/src/org/uva/sea/ql/check/CheckExpr.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/check/CheckExpr.java
@@ -5,20 +5,21 @@ import java.util.Map;
 
 import org.uva.sea.ql.ast.Expr;
 import org.uva.sea.ql.ast.Type;
-import org.uva.sea.ql.ast.expressions.binary.BinaryBoolExpr;
 import org.uva.sea.ql.ast.expressions.binary.BinaryArithmeticExpr;
+import org.uva.sea.ql.ast.expressions.binary.BinaryRelationalExpr;
+import org.uva.sea.ql.ast.expressions.binary.BinaryLogicalExpr;
 import org.uva.sea.ql.ast.expressions.binary.arithmetic.Add;
 import org.uva.sea.ql.ast.expressions.binary.arithmetic.Div;
 import org.uva.sea.ql.ast.expressions.binary.arithmetic.Mul;
 import org.uva.sea.ql.ast.expressions.binary.arithmetic.Sub;
-import org.uva.sea.ql.ast.expressions.binary.bool.Eq;
-import org.uva.sea.ql.ast.expressions.binary.bool.GEq;
-import org.uva.sea.ql.ast.expressions.binary.bool.GT;
-import org.uva.sea.ql.ast.expressions.binary.bool.LEq;
-import org.uva.sea.ql.ast.expressions.binary.bool.LT;
-import org.uva.sea.ql.ast.expressions.binary.bool.NEq;
 import org.uva.sea.ql.ast.expressions.binary.logical.And;
 import org.uva.sea.ql.ast.expressions.binary.logical.Or;
+import org.uva.sea.ql.ast.expressions.binary.relational.Eq;
+import org.uva.sea.ql.ast.expressions.binary.relational.GEq;
+import org.uva.sea.ql.ast.expressions.binary.relational.GT;
+import org.uva.sea.ql.ast.expressions.binary.relational.LEq;
+import org.uva.sea.ql.ast.expressions.binary.relational.LT;
+import org.uva.sea.ql.ast.expressions.binary.relational.NEq;
 import org.uva.sea.ql.ast.expressions.literal.Bool;
 import org.uva.sea.ql.ast.expressions.literal.Ident;
 import org.uva.sea.ql.ast.expressions.literal.Int;
@@ -55,21 +56,22 @@ public class CheckExpr implements Visitor<Boolean> {
 	public Boolean visit(Mul ast) { return checkNumericExpr(ast, "*"); }
 
 	@Override
-	public Boolean visit(And ast) { return checkBooleanExpr(ast, "&&"); }
+	public Boolean visit(Eq ast)  { return checkRelationalExpr(ast, "=="); }
 	@Override
-	public Boolean visit(Eq ast)  { return checkBooleanExpr(ast, "=="); }
+	public Boolean visit(GEq ast) { return checkRelationalExpr(ast, ">="); }
 	@Override
-	public Boolean visit(GEq ast) { return checkBooleanExpr(ast, ">="); }
+	public Boolean visit(GT ast)  { return checkRelationalExpr(ast, ">");  }
 	@Override
-	public Boolean visit(GT ast)  { return checkBooleanExpr(ast, ">");  }
+	public Boolean visit(LEq ast) { return checkRelationalExpr(ast, "<="); }
 	@Override
-	public Boolean visit(LEq ast) { return checkBooleanExpr(ast, "<="); }
+	public Boolean visit(LT ast)  { return checkRelationalExpr(ast, "<");  }
 	@Override
-	public Boolean visit(LT ast)  { return checkBooleanExpr(ast, "<");  }
+	public Boolean visit(NEq ast) { return checkRelationalExpr(ast, "!="); }
+	
 	@Override
-	public Boolean visit(NEq ast) { return checkBooleanExpr(ast, "!="); }
+	public Boolean visit(And ast) { return checkLogicalExpr(ast, "&&"); }
 	@Override
-	public Boolean visit(Or ast)  { return checkBooleanExpr(ast, "||"); }
+	public Boolean visit(Or ast)  { return checkLogicalExpr(ast, "||"); }
 
 	@Override
 	public Boolean visit(Neg ast) {
@@ -141,7 +143,29 @@ public class CheckExpr implements Visitor<Boolean> {
 		return true; 
 	}
 	
-	private Boolean checkBooleanExpr(BinaryBoolExpr expr, String binarySymbol) {
+	private Boolean checkRelationalExpr(BinaryRelationalExpr expr, String binarySymbol) {
+		boolean checkLhs = expr.getLhs().accept(this);
+		boolean checkRhs = expr.getRhs().accept(this);
+		
+		if (!(checkLhs && checkRhs)) {
+			// Type errors occurred
+			return false;
+		}
+		
+		Type lhsType = expr.getLhs().typeOf(_typeEnv);
+		Type rhsType = expr.getRhs().typeOf(_typeEnv);
+		
+		// Check if Types are compatible with BinaryBoolExpr
+		if (!(lhsType.isCompatibleToBool() && rhsType.isCompatibleToBool())) {
+			addError(expr, "invalid type for " + binarySymbol);
+			return false;
+		}
+		
+		// No Type errors
+		return true; 
+	}
+	
+	private Boolean checkLogicalExpr(BinaryLogicalExpr expr, String binarySymbol) {
 		boolean checkLhs = expr.getLhs().accept(this);
 		boolean checkRhs = expr.getRhs().accept(this);
 		
