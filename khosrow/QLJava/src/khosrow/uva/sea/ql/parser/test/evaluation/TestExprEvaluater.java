5f324c64cc81be10a9c2d62c0063f2a0185a3133
Khosrow-Azizi <khosrowazizi@live.nl>
2013-02-11 14:04:34 +0100

    Removed type compatibility check in IntVal and MoneyVal.

--- khosrow/QLJava/src/khosrow/uva/sea/ql/parser/test/evaluation/TestExprEvaluater.java
+++ khosrow/QLJava/src/khosrow/uva/sea/ql/parser/test/evaluation/TestExprEvaluater.java
@@ -1,11 +1,12 @@
 package khosrow.uva.sea.ql.parser.test.evaluation;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import khosrow.uva.sea.ql.parser.jacc.JACCParser;
 import khosrow.uva.sea.ql.parser.test.IParse;
 import khosrow.uva.sea.ql.parser.test.ParseError;
 import khosrow.uva.sea.ql.parser.test.TheTestEnv;
-import khosrow.uva.sea.ql.values.*;
+import khosrow.uva.sea.ql.values.IntVal;
+import khosrow.uva.sea.ql.values.MoneyVal;
 import khosrow.uva.sea.ql.visitor.eval.ExprEvaluator;
 
 import org.junit.Before;
@@ -13,22 +14,21 @@ import org.junit.Test;
 
 public class TestExprEvaluater {
 	private IParse parser;
-	
+
 	@Before
 	public void setUp() {
 		parser = new JACCParser();
 	}
-	
+
 	@Test
 	public void testAddsVal() throws ParseError {
-		//assertEquals(((IntVal)ExprEvaluator.Evaluate(parser.ParseExpression("1 + 2"), TheTestEnv.getTypeEnv())).getValue(), new Integer(3));	
-		//assertEquals(((MoneyVal)ExprEvaluator.Evaluate(parser.ParseExpression("1 + 2.3"), TheTestEnv.getTypeEnv())).getValue(), new Double(3.3));
-		//assertEquals(((IntVal)ExprEvaluator.Evaluate(parser.ParseExpression("1 - 2"), TheTestEnv.getTypeEnv())).getValue(), new Integer(-1));	
-		//assertEquals(((MoneyVal)ExprEvaluator.Evaluate(parser.ParseExpression("1 * 2.3"), TheTestEnv.getTypeEnv())).getValue(), new Double(2.3));
-		//assertEquals(((IntVal)ExprEvaluator.Evaluate(parser.ParseExpression("1 / 2"), TheTestEnv.getTypeEnv())).getValue(), new Integer(1));	
-		assertEquals(((MoneyVal)ExprEvaluator.Evaluate(parser.ParseExpression("2.3 - 1.0"), TheTestEnv.getTypeEnv())).getValue(), new Double(1.3));
-		//assertEquals(((IntVal)ExprEvaluator.Evaluate(parser.ParseExpression("iA + iB"), TheTestEnv.getTypeEnv())).getValue(), new Integer(3));
-		//assertEquals(((IntVal)ExprEvaluator.Evaluate(parser.ParseExpression("iA + 1"), TheTestEnv.getTypeEnv())).getValue(), new Integer(2));
-		//assertEquals(((MoneyVal)ExprEvaluator.Evaluate(parser.ParseExpression("iA + mA"), TheTestEnv.getTypeEnv())).getValue(), new Double(2.1));
+		assertEquals(((IntVal)ExprEvaluator.Evaluate(parser.ParseExpression("1 + 2"), TheTestEnv.getTypeEnv())).getValue(), new Integer(3));	
+		assertEquals(((MoneyVal)ExprEvaluator.Evaluate(parser.ParseExpression("1 + 2.3"), TheTestEnv.getTypeEnv())).getValue(), new Double(3.3));
+		assertEquals(((IntVal)ExprEvaluator.Evaluate(parser.ParseExpression("1 - 2"), TheTestEnv.getTypeEnv())).getValue(), new Integer(-1));	
+		assertEquals(((MoneyVal)ExprEvaluator.Evaluate(parser.ParseExpression("1 * 2.3"), TheTestEnv.getTypeEnv())).getValue(), new Double(2.3));
+		assertEquals(((IntVal)ExprEvaluator.Evaluate(parser.ParseExpression("1 / 2"), TheTestEnv.getTypeEnv())).getValue(), new Integer(1/2));	
+		assertEquals(((MoneyVal)ExprEvaluator.Evaluate(parser.ParseExpression("2.3 - 1.0"), TheTestEnv.getTypeEnv())).getValue(), new Double(2.3 - 1.0));
+		assertEquals(((IntVal)ExprEvaluator.Evaluate(parser.ParseExpression("iA + iB"), TheTestEnv.getTypeEnv())).getValue(), 
+			new Integer(((IntVal)TheTestEnv.getValueOf("iA")).getValue() + ((IntVal)TheTestEnv.getValueOf("iB")).getValue()));				
 	}
 }
--- khosrow/QLJava/src/khosrow/uva/sea/ql/values/IntVal.java
+++ khosrow/QLJava/src/khosrow/uva/sea/ql/values/IntVal.java
@@ -2,7 +2,7 @@ package khosrow.uva.sea.ql.values;
 
 public class IntVal extends NumericVal {
 	private final Integer value;
-	
+
 	public IntVal(int value) {
 		this.value = value;
 	}
@@ -12,110 +12,126 @@ public class IntVal extends NumericVal {
 	}
 
 	@Override
-	public boolean isCompatibleToInt() { return true; }
-	
+	public boolean isCompatibleToInt() {
+		return true;
+	}
+
 	@Override
 	public int compareTo(Value value) {
-		return getValue() - ((IntVal)value).getValue();
+		return getValue() - ((IntVal) value).getValue();
 	}
-		
+
 	@Override
 	public Value add(Value arg) {
-		if(arg.isCompatibleToInt())
-			return arg.addInt(this);
-		return arg.add(this.convertToMoney());
+		return arg.addInt(this);
 	}
-	
+
 	@Override
 	public Value sub(Value arg) {
-		if(arg.isCompatibleToInt())
-			return arg.subInt(this);
-		return arg.sub(this.convertToMoney());
+		return arg.subInt(this);
 	}
-	
+
 	@Override
 	public Value mul(Value arg) {
-		if(arg.isCompatibleToInt())
-			return arg.mulInt(this);
-		return arg.mul(this.convertToMoney());
+		return arg.mulInt(this);
 	}
-	
+
 	@Override
 	public Value mod(Value arg) {
-		if(arg.isCompatibleToInt())
-			return arg.modInt(this);
-		return arg.mod(this.convertToMoney());
-	}	
-	
+		return arg.modInt(this);
+	}
+
 	@Override
 	public Value div(Value arg) {
-		if(arg.isCompatibleToInt())
-			return arg.divInt(this);
-		return arg.div(this.convertToMoney());
+		return arg.divInt(this);
 	}
-	
+
 	@Override
 	public Value pos() {
 		return this;
 	}
-	
+
 	@Override
 	public Value neg() {
 		return new IntVal(-1 * getValue());
 	}
-	
+
 	public Value eq(Value arg) {
-		//if(arg.isCompatibleToInt())
-			return new BoolVal((false));
+		return new BoolVal((false));
 	}
-	
+
 	public Value neq(Value arg) {
 		throw new UnsupportedOperationException();
 	}
-	
+
 	public Value geq(Value arg) {
 		throw new UnsupportedOperationException();
 	}
-	
+
 	public Value gt(Value arg) {
 		throw new UnsupportedOperationException();
 	}
-	
+
 	public Value leq(Value arg) {
 		throw new UnsupportedOperationException();
 	}
-	
+
 	public Value lt(Value arg) {
 		throw new UnsupportedOperationException();
 	}
-		
+
 	@Override
 	protected Value addInt(IntVal arg) {
 		return new IntVal(arg.getValue() + getValue());
 	}
-	
+
 	@Override
 	protected Value subInt(IntVal arg) {
 		return new IntVal(arg.getValue() - getValue());
 	}
-	
+
 	@Override
 	protected Value mulInt(IntVal arg) {
 		return new IntVal(arg.getValue() * getValue());
 	}
-	
+
 	@Override
 	protected Value divInt(IntVal arg) {
 		return new IntVal(arg.getValue() / getValue());
 	}
-	
+
 	@Override
 	protected Value modInt(IntVal arg) {
 		return new IntVal(arg.getValue() % getValue());
 	}
-	
+
+	@Override
+	protected Value addMoney(MoneyVal arg) {
+		return new MoneyVal(arg.getValue() + getValue());
+	}
+
+	@Override
+	protected Value subMoney(MoneyVal arg) {
+		return new MoneyVal(arg.getValue() - getValue());
+	}
+
+	@Override
+	protected Value mulMoney(MoneyVal arg) {
+		return new MoneyVal(arg.getValue() * getValue());
+	}
+
+	@Override
+	protected Value divMoney(MoneyVal arg) {
+		return new MoneyVal(arg.getValue() / getValue());
+	}
+
+	@Override
+	protected Value modMoney(MoneyVal arg) {
+		return new MoneyVal(arg.getValue() % getValue());
+	}
+
 	private Value convertToMoney() {
 		return new MoneyVal(getValue());
 	}
-	
+
 }
