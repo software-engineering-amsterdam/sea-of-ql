7c042498fc11780cf94354a1f6f3e37af35f64b1
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-11 11:32:49 +0100

    Ident literal expr now extends LiteralExpr super class

--- edwinvm/QLJava/src/org/uva/sea/ql/ast/expressions/literal/Ident.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/ast/expressions/literal/Ident.java
@@ -2,22 +2,16 @@ package org.uva.sea.ql.ast.expressions.literal;
 
 import java.util.Map;
 
-import org.uva.sea.ql.ast.Expr;
 import org.uva.sea.ql.ast.Type;
+import org.uva.sea.ql.ast.expressions.LiteralExpr;
 import org.uva.sea.ql.ast.types.Error;
 import org.uva.sea.ql.ast.visitors.checkexpr.Visitor;
 
-public class Ident extends Expr {
-	private final String _name;
+public class Ident extends LiteralExpr {
 
 	public Ident(String name) {
-		_name = name;
+		super(name);
 	}
-	
-	public String getName() {
-		return _name;
-	}
-
 	@Override
 	public Type typeOf(Map<Ident, Type> typeEnv) {
 		if (typeEnv.containsKey(this)) {
