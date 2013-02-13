30bbc55ed52dc13e8100d8b1a7c139770782e99d
Sven Meyer <meyer.sven@gmail.com>
2013-02-13 16:42:13 +0100

    ExpressionTypeChecker finished (untested), now starting on FormChecker)

--- sven-m/QLJava/src/org/uva/sea/ql/parser/errors/VariableRedefinitionError.java
+++ sven-m/QLJava/src/org/uva/sea/ql/parser/errors/VariableRedefinitionError.java
@@ -1,13 +1,12 @@
 package org.uva.sea.ql.parser.errors;
 
 import org.uva.sea.ql.ast.expression.Expr;
-import org.uva.sea.ql.ast.expression.Ident;
 
 public class VariableRedefinitionError extends SemanticError {
 	private final static String FORMAT = "Variable redefined: %s";
 	
-	public VariableRedefinitionError(Ident id, Expr expression) {
-		super(String.format(FORMAT, id), expression);
+	public VariableRedefinitionError(String varName, Expr expression) {
+		super(String.format(FORMAT, varName), expression);
 	}
 
 }
--- sven-m/QLJava/src/org/uva/sea/ql/parser/typechecking/FormTypeChecker.java
+++ sven-m/QLJava/src/org/uva/sea/ql/parser/typechecking/FormTypeChecker.java
@@ -5,25 +5,23 @@ import java.util.List;
 import java.util.Map;
 
 import org.uva.sea.ql.ast.visitor.FormVisitor;
+import org.uva.sea.ql.ast.form.BoolType;
 import org.uva.sea.ql.ast.form.Form;
 import org.uva.sea.ql.ast.form.Body;
 import org.uva.sea.ql.ast.form.IfStatement;
 import org.uva.sea.ql.ast.form.ElseIfStatement;
 import org.uva.sea.ql.ast.form.ElseStatement;
+import org.uva.sea.ql.ast.form.IntType;
 import org.uva.sea.ql.ast.form.Question;
 import org.uva.sea.ql.ast.form.Computed;
-import org.uva.sea.ql.ast.form.Type;
+import org.uva.sea.ql.ast.form.StrType;
 
 public class FormTypeChecker implements FormVisitor<Boolean> {
 	
-	private Map<String, Type> typeEnvironment;
-	private List<String> messages;
-
-	public FormTypeChecker(Map<String, Type> typeEnvironment,
-			List<String> messages)
-	{
-		this.typeEnvironment = typeEnvironment;
-		this.messages = messages;
+	private Environment environment;
+	
+	public FormTypeChecker(Environment environment) {
+		this.environment = environment;
 	}
 
 	@Override
@@ -63,6 +61,24 @@ public class FormTypeChecker implements FormVisitor<Boolean> {
 
 	@Override
 	public Boolean visit(Computed ast) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean visit(IntType ast) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean visit(BoolType ast) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean visit(StrType ast) {
 		// TODO Auto-generated method stub
 		return null;
 	}
