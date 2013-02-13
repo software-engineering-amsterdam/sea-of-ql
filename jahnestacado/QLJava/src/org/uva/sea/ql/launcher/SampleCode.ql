59874764df703372594a1de3ecfaa8c254debe6d
jahnestacado <ioannis.kiwe@gmail.com>
2013-02-13 22:57:36 +0100

    pdf generator layout fixed

--- jahnestacado/QLJava/src/org/uva/sea/ql/launcher/SampleCode.ql
+++ jahnestacado/QLJava/src/org/uva/sea/ql/launcher/SampleCode.ql
@@ -1,4 +1,8 @@
-form form1 {
+form B66ox1HouseOwning {
+	hasSoldHouse: "Did you sell a house in 2010?" int
+		t1: "Did you sell a house in 2010?" int (hasSoldHouse)
+	
+
 	age: "What is your age?" int
 	isGirl: "Are you a girl?" boolean
 	if (age >= 18 && isGirl) {
@@ -11,4 +15,10 @@ form form1 {
 	else {
 		lifeIsHard: "Go away" boolean(true)
 	}
+
+	
+	
+
+
+
 }
--- jahnestacado/QLJava/src/org/uva/sea/ql/output/generators/pdf/QLToPDF.java
+++ jahnestacado/QLJava/src/org/uva/sea/ql/output/generators/pdf/QLToPDF.java
@@ -3,7 +3,9 @@ package org.uva.sea.ql.output.generators.pdf;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
@@ -14,20 +16,34 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
  
 public class QLToPDF {
-	private final static Font font = new Font(Font.FontFamily.ZAPFDINGBATS, 13f, Font.BOLD);
-	private final static Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 20,Font.BOLD);
-    private static Font qFont = new Font(Font.FontFamily.TIMES_ROMAN, 16,Font.NORMAL);
-	//private final static String path = "src/outputgenerator/pdf/images/c1.png";
+	private final static Font symbolFont = new Font(Font.FontFamily.ZAPFDINGBATS, 15, Font.BOLD);
+	private final static Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,Font.UNDERLINE);
+    private static Font questionFont = new Font(Font.FontFamily.TIMES_ROMAN, 14,Font.ITALIC);
+    private static Font resultsFont = new Font(Font.FontFamily.TIMES_ROMAN, 14,Font.BOLD);
+    private final static String imgPath = "src/org/uva/sea/ql/output/generators/pdf/images/uva_logo.jpg";
+    private final static Map<String,String> boolValues;
+    static
+    {
+    	boolValues = new HashMap<String, String>();
+    	boolValues.put("true", "Yes");
+    	boolValues.put("false","No");
+    }
+    
+    private final static Map<String,String> yesOrNoSymbol;
+    static
+    {
+    	yesOrNoSymbol = new HashMap<String, String>();
+    	yesOrNoSymbol.put("true", "4");   //** Values '4' and '6' corresponds to 'tick' and 'X' symbols of
+    	yesOrNoSymbol.put("false","6");   //** the ZAPFDINGBATS font. 
+    }
 
 
     private final List<String> questionLabels;
     private final List<String> questionValues;
 	
-	final String yes="4";
-	final String no="6";
+	
 	
 	private QLToPDF(List<String> questionLabels,List<String> questionValues){
-	
 		this.questionLabels=questionLabels;
 		this.questionValues=questionValues;
 	}
@@ -39,38 +55,32 @@ public class QLToPDF {
     }
     
     
-    
-    private void putContent(String frameName){
-        try {
-			String path = "src/org/uva/sea/ql/output/pdf/"+frameName+".pdf";
+	private void putContent(String frameName) {
+		try {
+			String path = "src/org/uva/sea/ql/output/pdf/" + frameName + ".pdf";
 			File filePath = new File(path);
-            OutputStream file = new FileOutputStream(filePath.getAbsolutePath());
-            Document document = new Document();
-            PdfWriter.getInstance(document, file);
-            document.open();
-            document.add(setTitle(frameName));
+			OutputStream file = new FileOutputStream(filePath.getAbsolutePath());
+			Document document = new Document();
+			PdfWriter.getInstance(document, file);
+			document.open();
+			Image img = Image.getInstance(imgPath);
+			document.add(setHeaderLogo(img));
+			document.add(setTitle(frameName));
 
-            for(int i=0;i<=questionLabels.size()-1;i++){
-            Paragraph p1=new Paragraph(questionLabels.get(i),qFont);
-            p1.add(new Chunk("    "));
-           // p1.add(new Chunk(img,0,0,true));
-            p1.add(new Chunk(" "+questionValues.get(i)));
-            addEmptyLine(p1, 1);
+			for (int i = 0; i <= questionLabels.size() - 1; i++) {
+				Paragraph p1 = new Paragraph(questionLabels.get(i),
+						questionFont);
 
-
-            document.add(new Paragraph(p1));
-            
-            }
-          
- 
-            document.close();
-            file.close();
-
- 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+				p1 = getProperDisplayedValue(p1, questionValues.get(i));
+				addEmptyLine(p1, 1);
+				document.add(new Paragraph(p1));
+			}
+			document.close();
+			file.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
     
     private static void addEmptyLine(Paragraph paragraph, int number) {
         for (int i = 0; i < number; i++) {
@@ -78,14 +88,38 @@ public class QLToPDF {
         }
       }
     
-    private static Paragraph setTitle(String frameName){
-    	Paragraph header= new Paragraph();
-        addEmptyLine(header, 1);
-        Paragraph title= new Paragraph(frameName, catFont);
-        header.add(title);
-        addEmptyLine(header, 4);
-        title.setAlignment(Element.ALIGN_CENTER);
-        return header;
-    }
+	private static Paragraph setHeaderLogo(Image img) {
+		Paragraph logoHeader = new Paragraph();
+		img.scalePercent(15f);
+		Chunk imgChunk = new Chunk(img, 0, 0, true);
+		logoHeader.add(imgChunk);
+		logoHeader.setAlignment(Element.ALIGN_LEFT);
+		addEmptyLine(logoHeader, 1);
+		return logoHeader;
+	}
+    
+	private static Paragraph setTitle(String frameName) {
+		Paragraph header = new Paragraph();
+		addEmptyLine(header, 1);
+		Paragraph title = new Paragraph(frameName + " Questionnaire", titleFont);
+		header.add(title);
+		addEmptyLine(header, 2);
+		title.setAlignment(Element.ALIGN_CENTER);
+		return header;
+	}
+    
+	private Paragraph getProperDisplayedValue(Paragraph p, String value) {
+		p.add(new Chunk("     "));
+		if (boolValues.containsKey(value)) {
+			p.add(new Chunk(yesOrNoSymbol.get(value), symbolFont));
+			p.add(new Chunk("  (" + boolValues.get(value) + ")", resultsFont));
+		} else {
+			p.add(new Chunk("  " + value, resultsFont));
+
+		}
+
+		return p;
+
+	}
    
 }
