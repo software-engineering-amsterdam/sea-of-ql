84a1842d0570a9a06b47a6a957260f3c4ef67307
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-14 01:58:31 +0100

    Added typeEnv as a first class collection class

---
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/SupportedTypes.java
@@ -0,0 +1,19 @@
+package org.uva.sea.ql.parser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.uva.sea.ql.ast.Type;
+import org.uva.sea.ql.ast.expressions.literal.Ident;
+
+public class SupportedTypes {
+	private final Map<Ident, Type> _supportedTypes;
+
+	public SupportedTypes() {
+		_supportedTypes = new HashMap<Ident, Type>();
+	}
+	
+	public void add(String identifier, Type type) {
+		_supportedTypes.put(new Ident(identifier), type);
+	}
+}
