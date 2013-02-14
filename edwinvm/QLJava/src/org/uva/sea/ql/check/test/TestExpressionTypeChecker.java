ab1232620ef5e24b0ccf72a87bfe3cb65b9dd51a
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-14 01:21:14 +0100

    Refactored binary numeric expr type checker, now extends super class

--- edwinvm/QLJava/src/org/uva/sea/ql/check/test/TestExpressionTypeChecker.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/check/test/TestExpressionTypeChecker.java
@@ -1,46 +1,19 @@
 package org.uva.sea.ql.check.test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 import org.junit.Test;
-import org.uva.sea.ql.ast.Expr;
-import org.uva.sea.ql.ast.Type;
 import org.uva.sea.ql.ast.expressions.binary.arithmetic.Add;
 import org.uva.sea.ql.ast.expressions.binary.arithmetic.Div;
 import org.uva.sea.ql.ast.expressions.binary.arithmetic.Mul;
 import org.uva.sea.ql.ast.expressions.binary.arithmetic.Sub;
-import org.uva.sea.ql.ast.expressions.literal.Ident;
-import org.uva.sea.ql.ast.types.Bool;
-import org.uva.sea.ql.ast.types.Int;
-import org.uva.sea.ql.ast.types.Money;
-import org.uva.sea.ql.ast.types.Numeric;
-import org.uva.sea.ql.ast.types.Str;
-import org.uva.sea.ql.check.CheckExpr;
 import org.uva.sea.ql.parser.IParser;
-import org.uva.sea.ql.parser.Message;
 import org.uva.sea.ql.parser.ParseError;
 import org.uva.sea.ql.parser.antlr.check.ANTLRParserExpressions;
 
-public class TestExpressionTypeChecker {
+public class TestExpressionTypeChecker extends ExpressionTypeChecker {
 	private IParser _parser;
-	private HashMap<Ident, Type> _typeEnv;
-	private List<Message> _errorMessages;
 	
 	public TestExpressionTypeChecker() {
 		_parser = new ANTLRParserExpressions();
-		_errorMessages = new ArrayList<Message>();
-		_typeEnv = new HashMap<Ident, Type>();
-		
-		_typeEnv.put(new Ident("bool"),    new Bool());
-		_typeEnv.put(new Ident("int"),     new Int());
-		_typeEnv.put(new Ident("money"),   new Money());
-		_typeEnv.put(new Ident("numeric"), new Numeric());
-		_typeEnv.put(new Ident("str"),     new Str());
 	}
 	
 	@Test
@@ -86,17 +59,5 @@ public class TestExpressionTypeChecker {
 		isNotAValidExpression((Sub) _parser.parse("1 - a"));
 		isNotAValidExpression((Sub) _parser.parse("1 - true"));
 		isNotAValidExpression((Sub) _parser.parse("true - true"));
-	}
-	
-	private void isAValidExpression(Expr expr) {
-		assertTrue(checkExpression(expr));
-	}
-	
-	private void isNotAValidExpression(Expr expr) {
-		assertFalse(checkExpression(expr));
-	}
-	
-	private Boolean checkExpression(Expr expr) {
-		return CheckExpr.check(expr, _typeEnv, _errorMessages);
 	}
 }
