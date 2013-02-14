de6613c83e801ccad8324531de97bfd034c4fa74
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-14 02:14:47 +0100

    Added helper methods for SupportedTypes class

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/SupportedTypes.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/SupportedTypes.java
@@ -13,7 +13,15 @@ public class SupportedTypes {
 		_supportedTypes = new HashMap<Ident, Type>();
 	}
 	
+	public Boolean contains(Ident identifier) {
+		return _supportedTypes.containsKey(identifier);
+	}
+	
 	public void add(String identifier, Type type) {
 		_supportedTypes.put(new Ident(identifier), type);
+	}
+	
+	public Type get(Ident identifier) {
+		return _supportedTypes.get(identifier);
 	}
 }
