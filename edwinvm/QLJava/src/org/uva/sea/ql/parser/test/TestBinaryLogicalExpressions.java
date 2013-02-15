e3c109cc9431bfaf220aa9643f28483cccaa4159
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-15 19:20:28 +0100

    Subtracted relational unit tests into new Unit Test class

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestBinaryLogicalExpressions.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestBinaryLogicalExpressions.java
@@ -5,12 +5,6 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.uva.sea.ql.ast.expressions.binary.logical.And;
 import org.uva.sea.ql.ast.expressions.binary.logical.Or;
-import org.uva.sea.ql.ast.expressions.binary.relational.Eq;
-import org.uva.sea.ql.ast.expressions.binary.relational.GEq;
-import org.uva.sea.ql.ast.expressions.binary.relational.GT;
-import org.uva.sea.ql.ast.expressions.binary.relational.LEq;
-import org.uva.sea.ql.ast.expressions.binary.relational.LT;
-import org.uva.sea.ql.ast.expressions.binary.relational.NEq;
 import org.uva.sea.ql.parser.IParser;
 import org.uva.sea.ql.parser.ParseError;
 import org.uva.sea.ql.parser.antlr.check.ANTLRParserExpressions;
@@ -21,22 +15,6 @@ public class TestBinaryLogicalExpressions {
 	
 	public TestBinaryLogicalExpressions() {
 		_parser = new ANTLRParserExpressions();
-	}
-	
-	@Test
-	public void testRels() throws ParseError {
-		assertEquals(_parser.parse("a < b").getClass(), LT.class);
-		assertEquals(_parser.parse("a < b + c").getClass(), LT.class);
-		assertEquals(_parser.parse("a < (b * c)").getClass(), LT.class);
-		assertEquals(_parser.parse("(a * b) < c").getClass(), LT.class);
-		
-		assertEquals(_parser.parse("(a == b)").getClass(), Eq.class);
-		assertEquals(_parser.parse("(a <= b)").getClass(), LEq.class);
-		assertEquals(_parser.parse("(a >= b)").getClass(), GEq.class);
-		assertEquals(_parser.parse("(a != b)").getClass(), NEq.class);
-		
-		assertEquals(_parser.parse("a + b > c").getClass(), GT.class);
-		assertEquals(_parser.parse("a > b + c").getClass(), GT.class);
 	}
 	
 	@Test
