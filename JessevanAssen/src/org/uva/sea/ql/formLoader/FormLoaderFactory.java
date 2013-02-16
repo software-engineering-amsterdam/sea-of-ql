311ced54aa170427cb9f04285bd96d7156a106ca
Jesse van Assen <jesse.v.assen@gmail.com>
2013-02-16 12:37:55 +0100

    Now the controller is responsible for defining the file instead of the factory. This is handy because I need to use servlet stuff in determining the path.

--- JessevanAssen/src/org/uva/sea/ql/formLoader/FormLoaderFactory.java
+++ JessevanAssen/src/org/uva/sea/ql/formLoader/FormLoaderFactory.java
@@ -6,9 +6,7 @@ import java.io.File;
 
 public class FormLoaderFactory {
 
-    public static FormLoader createFormLoader() {
-        File formFile = new File("C:\\Users\\Jesse\\workspace\\sea-of-ql\\JessevanAssen\\Box1HouseOwning.ql");
-
+    public static FormLoader createFormLoaderFromFile(File formFile) {
         return new FormLoaderImpl(
                 new FileFormSource(formFile),
                 ParserFactory.createParser());
