3f82e2f753525d50ca1e3f608b02612271eb0aa6
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-17 00:42:16 +0100

    Refactored TestForms unit test to use new check

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestForms.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestForms.java
@@ -1,7 +1,6 @@
 package org.uva.sea.ql.parser.test;
 
 import org.junit.Test;
-import org.uva.sea.ql.ast.Form;
 import org.uva.sea.ql.parser.ParseError;
 import org.uva.sea.ql.parser.antlr.check.ANTLRParserForms;
 
@@ -12,16 +11,14 @@ public class TestForms extends ASTNodeChecker {
 	
 	@Test
 	public void testFormSingleQuestion() throws ParseError {
-		expressionMatchesASTClass("form boxHouse1Owning { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }", Form.class);
+		inputMatchesFormAST("form boxHouse1Owning { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }");
 	}
-	
 	@Test
 	public void testFormMultipleQuestions() throws ParseError {
-		expressionMatchesASTClass("form boxHouse1Owning { \"Did you sell a house in 2010?\" hasSoldHouse: boolean \"Price the house was sold for:\" sellingPrice: integer }", Form.class);
+		inputMatchesFormAST("form boxHouse1Owning { \"Did you sell a house in 2010?\" hasSoldHouse: boolean \"Price the house was sold for:\" sellingPrice: integer }");
 	}
-	
 	@Test
 	public void testFormWithIfStatement() throws ParseError {
-		expressionMatchesASTClass("form boxHouse1Owning { \"Did you sell a house in 2010?\" hasSoldHouse: boolean if (hasSoldHouse) { \"Qualifies for loan:\" qualifiesForLoan = \"true\" } }", Form.class);
+		inputMatchesFormAST("form boxHouse1Owning { \"Did you sell a house in 2010?\" hasSoldHouse: boolean if (hasSoldHouse) { \"Qualifies for loan:\" qualifiesForLoan = \"true\" } }");
 	}
 }
