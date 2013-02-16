868b0a98ec91939c4af71a09bd07e7d46d616729
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-16 23:21:33 +0100

    Refactored BinaryRelExpr unit test to use super class

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestBinaryRelationalExpressions.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestBinaryRelationalExpressions.java
@@ -1,7 +1,5 @@
 package org.uva.sea.ql.parser.test;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.uva.sea.ql.ast.expressions.binary.relational.Eq;
 import org.uva.sea.ql.ast.expressions.binary.relational.GEq;
@@ -9,31 +7,28 @@ import org.uva.sea.ql.ast.expressions.binary.relational.GT;
 import org.uva.sea.ql.ast.expressions.binary.relational.LEq;
 import org.uva.sea.ql.ast.expressions.binary.relational.LT;
 import org.uva.sea.ql.ast.expressions.binary.relational.NEq;
-import org.uva.sea.ql.parser.IParser;
 import org.uva.sea.ql.parser.ParseError;
 import org.uva.sea.ql.parser.antlr.check.ANTLRParserExpressions;
 
-public class TestBinaryRelationalExpressions {
+public class TestBinaryRelationalExpressions extends ASTNodeChecker {
 
-	private IParser _parser;
-	
 	public TestBinaryRelationalExpressions() {
-		_parser = new ANTLRParserExpressions();
+		super(new ANTLRParserExpressions());
 	}
 	
 	@Test
 	public void testRels() throws ParseError {
-		assertEquals(_parser.parse("a < b").getClass(), LT.class);
-		assertEquals(_parser.parse("a < b + c").getClass(), LT.class);
-		assertEquals(_parser.parse("a < (b * c)").getClass(), LT.class);
-		assertEquals(_parser.parse("(a * b) < c").getClass(), LT.class);
+		expressionMatchesASTClass("a < b",       LT.class);
+		expressionMatchesASTClass("a < b + c",   LT.class);
+		expressionMatchesASTClass("a < (b * c)", LT.class);
+		expressionMatchesASTClass("(a * b) < c", LT.class);
 		
-		assertEquals(_parser.parse("(a == b)").getClass(), Eq.class);
-		assertEquals(_parser.parse("(a <= b)").getClass(), LEq.class);
-		assertEquals(_parser.parse("(a >= b)").getClass(), GEq.class);
-		assertEquals(_parser.parse("(a != b)").getClass(), NEq.class);
+		expressionMatchesASTClass("(a == b)", Eq.class);
+		expressionMatchesASTClass("(a <= b)", LEq.class);
+		expressionMatchesASTClass("(a >= b)", GEq.class);
+		expressionMatchesASTClass("(a != b)", NEq.class);
 		
-		assertEquals(_parser.parse("a + b > c").getClass(), GT.class);
-		assertEquals(_parser.parse("a > b + c").getClass(), GT.class);
+		expressionMatchesASTClass("a + b > c", GT.class);
+		expressionMatchesASTClass("a > b + c", GT.class);
 	}
 }
