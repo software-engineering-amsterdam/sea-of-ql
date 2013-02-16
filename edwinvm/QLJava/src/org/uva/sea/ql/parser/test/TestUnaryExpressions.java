fcfe0c1c62d9c2cd13f67ab7f966765a049519e3
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-16 23:28:45 +0100

    Refactored UnaryExpr unit test to use super class

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestUnaryExpressions.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestUnaryExpressions.java
@@ -1,35 +1,30 @@
 package org.uva.sea.ql.parser.test;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.uva.sea.ql.ast.expressions.unary.Neg;
 import org.uva.sea.ql.ast.expressions.unary.Not;
 import org.uva.sea.ql.ast.expressions.unary.Pos;
-import org.uva.sea.ql.parser.IParser;
 import org.uva.sea.ql.parser.ParseError;
 import org.uva.sea.ql.parser.antlr.check.ANTLRParserExpressions;
 
-public class TestUnaryExpressions {
+public class TestUnaryExpressions extends ASTNodeChecker {
 
-	private IParser _parser;
-	
 	public TestUnaryExpressions() {
-		_parser = new ANTLRParserExpressions();
+		super(new ANTLRParserExpressions());
 	}
 	
 	@Test
 	public void testNegs() throws ParseError {
-		assertEquals(_parser.parse("--x").getClass(), Neg.class);
+		expressionMatchesASTClass("--x", Neg.class);
 	}
 	
 	@Test
 	public void testPoss() throws ParseError {
-		assertEquals(_parser.parse("++x").getClass(), Pos.class);
+		expressionMatchesASTClass("++x", Pos.class);
 	}
 	
 	@Test
 	public void testNots() throws ParseError {
-		assertEquals(_parser.parse("!hasBoughtHouse").getClass(), Not.class);
+		expressionMatchesASTClass("!hasBoughtHouse", Not.class);
 	}
 }
