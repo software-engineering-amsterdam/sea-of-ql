ce508c5f7bac5fb28f74ea69b956ca02f17521fb
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-17 00:09:38 +0100

    Refactored TestForms unit test to use super class

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestForms.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestForms.java
@@ -1,32 +1,27 @@
 package org.uva.sea.ql.parser.test;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.uva.sea.ql.ast.Form;
-import org.uva.sea.ql.parser.IParser;
 import org.uva.sea.ql.parser.ParseError;
 import org.uva.sea.ql.parser.antlr.check.ANTLRParserForms;
 
-public class TestForms {
-	private IParser _parser;
-	
+public class TestForms extends ASTNodeChecker {
 	public TestForms() {
-		_parser = new ANTLRParserForms();
+		super(new ANTLRParserForms());
 	}
 	
 	@Test
 	public void testFormSingleQuestion() throws ParseError {
-		assertEquals(_parser.parse("form boxHouse1Owning { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }").getClass(), Form.class);
+		expressionMatchesASTClass("form boxHouse1Owning { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }", Form.class);
 	}
 	
 	@Test
 	public void testFormMultipleQuestions() throws ParseError {
-		assertEquals(_parser.parse("form boxHouse1Owning { \"Did you sell a house in 2010?\" hasSoldHouse: boolean \"Price the house was sold for:\" sellingPrice: integer }").getClass(), Form.class);
+		expressionMatchesASTClass("form boxHouse1Owning { \"Did you sell a house in 2010?\" hasSoldHouse: boolean \"Price the house was sold for:\" sellingPrice: integer }", Form.class);
 	}
 	
 	@Test
 	public void testFormWithIfStatement() throws ParseError {
-		assertEquals(_parser.parse("form boxHouse1Owning { \"Did you sell a house in 2010?\" hasSoldHouse: boolean if (hasSoldHouse) { \"Qualifies for loan:\" qualifiesForLoan = \"true\" } }").getClass(), Form.class);
+		expressionMatchesASTClass("form boxHouse1Owning { \"Did you sell a house in 2010?\" hasSoldHouse: boolean if (hasSoldHouse) { \"Qualifies for loan:\" qualifiesForLoan = \"true\" } }", Form.class);
 	}
 }
