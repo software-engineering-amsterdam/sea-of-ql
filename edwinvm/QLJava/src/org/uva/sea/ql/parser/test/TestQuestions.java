3d98c265e954c82e07f60fce4d26f053108d0870
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-17 00:13:09 +0100

    Refactored TestQuestions unit test to use super class

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestQuestions.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestQuestions.java
@@ -1,32 +1,27 @@
 package org.uva.sea.ql.parser.test;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.uva.sea.ql.ast.statements.questions.AnswerableQuestion;
 import org.uva.sea.ql.ast.statements.questions.ComputedQuestion;
-import org.uva.sea.ql.parser.IParser;
 import org.uva.sea.ql.parser.ParseError;
 import org.uva.sea.ql.parser.antlr.check.ANTLRParserQuestions;
 
-public class TestQuestions {
-	private IParser _parser;
-	
+public class TestQuestions extends ASTNodeChecker {
 	public TestQuestions() {
-		_parser = new ANTLRParserQuestions();
+		super(new ANTLRParserQuestions());
 	}
 	
 	@Test
 	public void testQuestions() throws ParseError {
-		assertEquals(_parser.parse("\"Did you sell a house in 2010?\" hasSoldHouse: boolean").getClass(), AnswerableQuestion.class);
-		assertEquals(_parser.parse("\"Price the house was sold for:\" sellingPrice: integer").getClass(), AnswerableQuestion.class);
-		assertEquals(_parser.parse("\"What is your name?\" userName: string").getClass(), AnswerableQuestion.class);
+		expressionMatchesASTClass("\"Did you sell a house in 2010?\" hasSoldHouse: boolean", AnswerableQuestion.class);
+		expressionMatchesASTClass("\"Price the house was sold for:\" sellingPrice: integer", AnswerableQuestion.class);
+		expressionMatchesASTClass("\"What is your name?\" userName: string", AnswerableQuestion.class);
 	}
 	
 	@Test
 	public void testComputedQuestions() throws ParseError {
-		assertEquals(_parser.parse("\"Did you sell a house in 2010?\" valueResidue = sellingPrice - privateDebt").getClass(), ComputedQuestion.class);
-		assertEquals(_parser.parse("\"Your profit percentage: \" percentageResidue = sellingPrice * 100 / privateDebt").getClass(), ComputedQuestion.class);
-		assertEquals(_parser.parse("\"Full name:\" fullName = firstName + lastName").getClass(), ComputedQuestion.class);
+		expressionMatchesASTClass("\"Did you sell a house in 2010?\" valueResidue = sellingPrice - privateDebt", ComputedQuestion.class);
+		expressionMatchesASTClass("\"Your profit percentage: \" percentageResidue = sellingPrice * 100 / privateDebt", ComputedQuestion.class);
+		expressionMatchesASTClass("\"Full name:\" fullName = firstName + lastName", ComputedQuestion.class);
 	}
 }
