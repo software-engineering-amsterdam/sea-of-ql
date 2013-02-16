23e8c06f7f33b712d1f9d2b3aa4ed694f8afbf9b
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-17 00:32:36 +0100

    Refactored ConditionBlock unit test to extend StatementParserChecker super class

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestConditionBlocks.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/TestConditionBlocks.java
@@ -1,11 +1,10 @@
 package org.uva.sea.ql.parser.test;
 
 import org.junit.Test;
-import org.uva.sea.ql.ast.statements.ConditionBlock;
 import org.uva.sea.ql.parser.ParseError;
 import org.uva.sea.ql.parser.antlr.check.ANTLRParserConditionBlocks;
 
-public class TestConditionBlocks extends ASTNodeChecker {
+public class TestConditionBlocks extends StatementParserChecker {
 	
 	public TestConditionBlocks() {
 		super(new ANTLRParserConditionBlocks());
@@ -14,38 +13,38 @@ public class TestConditionBlocks extends ASTNodeChecker {
 	@Test
 	public void testIfBlock() throws ParseError {
 		// Single If-Then condition block with Question as body
-		expressionMatchesASTClass("if (true) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }", ConditionBlock.class);
-		expressionMatchesASTClass("if (wantsToBuyHouse) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }", ConditionBlock.class);
-		expressionMatchesASTClass("if (wantsToBuyHouse && age >= 18) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }", ConditionBlock.class);
+		statementMatchesConditionBlock("if (true) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }");
+		statementMatchesConditionBlock("if (wantsToBuyHouse) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }");
+		statementMatchesConditionBlock("if (wantsToBuyHouse && age >= 18) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }");
 	}
 	
 	@Test
 	public void testIfBlockWithoutCurlyBrackets() throws ParseError {
 		// Single If-Then condition block without curly brackets
-		expressionMatchesASTClass("if (true) \"Did you sell a house in 2010?\" hasSoldHouse: boolean", ConditionBlock.class);
+		statementMatchesConditionBlock("if (true) \"Did you sell a house in 2010?\" hasSoldHouse: boolean");
 	}
 	
 	@Test
 	public void testElseBlock() throws ParseError {
 		// Single If-Then-Else condition block with Question as body
-		expressionMatchesASTClass("if (true) { \"Age?\" age: integer } else { \"Gender?\" gender: string }", ConditionBlock.class);
+		statementMatchesConditionBlock("if (true) { \"Age?\" age: integer } else { \"Gender?\" gender: string }");
 	}
 	
 	@Test
 	public void testElseBlockWithoutCurlyBrackets() throws ParseError {
 		// Single If-Then-Else condition block without curly brackets
-		expressionMatchesASTClass("if (true) \"Age?\" age: integer else \"Gender?\" gender: string", ConditionBlock.class);
+		statementMatchesConditionBlock("if (true) \"Age?\" age: integer else \"Gender?\" gender: string");
 	}
 	
 	@Test
 	public void testIfBlockWithMultipleBodyLines() throws ParseError {
 		// Single If-Then condition block with multiple lines as body
-		expressionMatchesASTClass("if (true) { \"Age?\" age: integer \n \"Name?\" name: string }", ConditionBlock.class);
+		statementMatchesConditionBlock("if (true) { \"Age?\" age: integer \n \"Name?\" name: string }");
 	}
 	
 	@Test
 	public void testInnerIfConditionBlocks() throws ParseError {
 		// Single If-Then condition block with inner If-Then condition block
-		expressionMatchesASTClass("if (true) { if (wantsToBuyHouse) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean } }", ConditionBlock.class);
+		statementMatchesConditionBlock("if (true) { if (wantsToBuyHouse) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean } }");
 	}
 }
