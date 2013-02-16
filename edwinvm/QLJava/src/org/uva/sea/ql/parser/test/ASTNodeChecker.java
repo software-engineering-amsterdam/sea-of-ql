5b06a6a2989c31426ff464a870426d7972efa8b0
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-17 00:41:43 +0100

    Added new check for forms in ASTNodeChecker super class

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/ASTNodeChecker.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/ASTNodeChecker.java
@@ -3,6 +3,7 @@ package org.uva.sea.ql.parser.test;
 import static org.junit.Assert.assertEquals;
 
 import org.uva.sea.ql.ast.ASTNode;
+import org.uva.sea.ql.ast.Form;
 import org.uva.sea.ql.parser.IParser;
 import org.uva.sea.ql.parser.ParseError;
 
@@ -12,6 +13,10 @@ public class ASTNodeChecker {
 	
 	public ASTNodeChecker(IParser parser) {
 		_parser = parser;
+	}
+	
+	public void inputMatchesFormAST(String input) throws ParseError {
+		assertEquals(getAstNodeFor(input), Form.class);
 	}
 	
 	public void expressionMatchesASTClass(String input, Class<?> astNode) throws ParseError {
