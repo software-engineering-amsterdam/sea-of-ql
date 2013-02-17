bc383ea6bc04a051f31c4d7e60b0237bc3611eef
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-17 01:14:41 +0100

    Made StatementParserChecker constructor explicit in usage

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/StatementParserChecker.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/StatementParserChecker.java
@@ -3,14 +3,14 @@ package org.uva.sea.ql.parser.test;
 import org.uva.sea.ql.ast.statements.ConditionBlock;
 import org.uva.sea.ql.ast.statements.questions.AnswerableQuestion;
 import org.uva.sea.ql.ast.statements.questions.ComputedQuestion;
-import org.uva.sea.ql.parser.IParser;
 import org.uva.sea.ql.parser.ParseError;
+import org.uva.sea.ql.parser.antlr.check.ANTLRParserConditionBlocks;
+import org.uva.sea.ql.parser.antlr.check.ANTLRParserQuestions;
 
 public class StatementParserChecker extends ASTNodeChecker {
 	
-	public StatementParserChecker(IParser parser) {
-		super(parser);
-	}
+	public StatementParserChecker(ANTLRParserConditionBlocks parser) { super(parser); }
+	public StatementParserChecker(ANTLRParserQuestions parser)       { super(parser); }
 	
 	public void statementMatchesConditionBlock(String input)     throws ParseError { match(input, ConditionBlock.class);     }
 	public void statementMatchesAnswerableQuestion(String input) throws ParseError { match(input, AnswerableQuestion.class); }
