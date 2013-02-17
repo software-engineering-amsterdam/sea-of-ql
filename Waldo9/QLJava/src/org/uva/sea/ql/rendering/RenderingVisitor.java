679fa6cdf9ff52b301a570131635cbbf1cfb8bc3
Waldo9 <contact@wouterkwakernaak.eu>
2013-02-17 17:30:56 +0100

    Updated rendering, started observer pattern

--- Waldo9/QLJava/src/org/uva/sea/ql/rendering/RenderingVisitor.java
+++ Waldo9/QLJava/src/org/uva/sea/ql/rendering/RenderingVisitor.java
@@ -1,66 +1,115 @@
 package org.uva.sea.ql.rendering;
 
-import java.awt.GridLayout;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 import javax.swing.JCheckBox;
-import javax.swing.JFrame;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
 
 import org.uva.sea.ql.ast.expressions.StringLiteral;
 import org.uva.sea.ql.ast.statements.Block;
 import org.uva.sea.ql.ast.statements.ComputedQuestion;
 import org.uva.sea.ql.ast.statements.IfThenElse;
 import org.uva.sea.ql.ast.statements.Question;
+import org.uva.sea.ql.ast.statements.Statement;
 import org.uva.sea.ql.ast.statements.StatementVisitor;
+import org.uva.sea.ql.ast.types.BoolType;
+import org.uva.sea.ql.ast.types.Type;
 
-public class RenderingVisitor implements StatementVisitor<Object> {
+public class RenderingVisitor implements StatementVisitor<Object>, FocusListener {
 	
-	private final JPanel contentPane;
+	private final JPanel panel;
+	private final State state;
 	
-	public RenderingVisitor() {
-		contentPane = new JPanel(new GridLayout(0,2));
+	public RenderingVisitor(State state) {
+		panel = new JPanel(new MigLayout("", "350", ""));
+		this.state = state;
+	}	
+
+	public static JPanel render(Statement statement, State state) {
+		RenderingVisitor renderingVisitor = new RenderingVisitor(state);
+		if (statement != null)
+			statement.accept(renderingVisitor);
+		return renderingVisitor.getPanel();
 	}
-	
-	public void testRender() {
-		JFrame frame = new JFrame("QL demo");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		contentPane.add(new JLabel("test label 1: "));
-		contentPane.add(new JTextField());
-		contentPane.add(new JLabel("test label 2:zzzzzz "));		
-		contentPane.add(new JCheckBox());
-		frame.setContentPane(contentPane);
-		frame.pack();
-		frame.setVisible(true);
+
+	public JPanel getPanel() {
+		return panel;
 	}
 
 	@Override
 	public Object visit(Question question) {
 		addLabel(question.getLabel());
-		return null;
-	}	
-	
-	@Override
-	public Object visit(Block block) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object visit(IfThenElse ifThenElse) {
-		// TODO Auto-generated method stub
+		JComponent control = typeToControl(question.getType());
+		registerEventHandler(question, control);
+		add(control);
 		return null;
 	}	
 
 	@Override
 	public Object visit(ComputedQuestion computedQuestion) {
-		// TODO Auto-generated method stub
+		addLabel(computedQuestion.getLabel());
+		JComponent control = typeToControl(computedQuestion.getType());
+		add(control);
 		return null;
+	}	
+
+	@Override
+	public Object visit(Block block) {
+		for (Statement statement : block.getStatements())
+			statement.accept(this);
+		return null;
+	}
+
+	@Override
+	public Object visit(IfThenElse ifThenElse) {
+		JPanel conditionTrue = render(ifThenElse.getBody(), state);
+		JPanel conditionFalse = render(ifThenElse.getElseBody(), state);
+		conditionTrue.setVisible(false);
+		conditionFalse.setVisible(false);
+		addPanel(conditionTrue);
+		addPanel(conditionFalse);
+		return null;
+	}	
+	
+	private void addPanel(JPanel panel) {
+		this.panel.add(panel, "span");
+	}
+
+	private void add(JComponent control) {
+		panel.add(control, "wrap");
+	}
+	
+	private void registerEventHandler(Question question, JComponent control) {
+		control.addFocusListener(this);
+	}
+
+	private JComponent typeToControl(Type type) {
+		if (type instanceof BoolType) {
+			return new JCheckBox();
+		}
+		return new JTextField(15);
 	}
 	
 	private void addLabel(StringLiteral label) {
-		contentPane.add(new JLabel(label.getValue()));		
+		panel.add(new JLabel(label.getValue()));
+	}
+
+	@Override
+	public void focusGained(FocusEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void focusLost(FocusEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
