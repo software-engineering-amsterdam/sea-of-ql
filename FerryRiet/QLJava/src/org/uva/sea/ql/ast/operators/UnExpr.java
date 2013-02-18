4b16861e986efcf4bc33f6a7ba24d5c0b6c1acf7
Ferry <Ferry>
2013-02-18 16:20:19 +0100

    Going to better visitors

--- FerryRiet/QLJava/src/org/uva/sea/ql/ast/operators/UnExpr.java
+++ FerryRiet/QLJava/src/org/uva/sea/ql/ast/operators/UnExpr.java
@@ -1,7 +1,5 @@
 package org.uva.sea.ql.ast.operators;
 
-import org.uva.sea.ql.ast.visitor.Visitor;
-
 public abstract class UnExpr extends Expr {
 	private final Expr exprRightHand;
 
@@ -11,10 +9,5 @@ public abstract class UnExpr extends Expr {
 
 	public Expr getExprRightHand() {
 		return exprRightHand;
-	}
-
-	@Override
-	public <T> T accept(Visitor<T> visitor) {
-		return visitor.visit(this);
 	}
 }
