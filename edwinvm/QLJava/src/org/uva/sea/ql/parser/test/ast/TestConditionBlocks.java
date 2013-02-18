0605947c4fbca98640ec22968e4ca0de43a5bd18
Edwin van Manen <edwin.vanmanen@student.uva.nl>
2013-02-18 17:30:10 +0100

    Renamed methods in ConditionBlock statement checker unit test

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/ast/TestConditionBlocks.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/ast/TestConditionBlocks.java
@@ -13,38 +13,38 @@ public class TestConditionBlocks extends StatementParserChecker {
 	@Test
 	public void testIfBlock() throws ParseError {
 		// Single If-Then condition block with Question as body
-		statementMatchesConditionBlock("if (true) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }");
-		statementMatchesConditionBlock("if (wantsToBuyHouse) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }");
-		statementMatchesConditionBlock("if (wantsToBuyHouse && age >= 18) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }");
+		statementMatchesIfThenCondition("if (true) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }");
+		statementMatchesIfThenCondition("if (wantsToBuyHouse) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }");
+		statementMatchesIfThenCondition("if (wantsToBuyHouse && age >= 18) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }");
 	}
 	
 	@Test
 	public void testIfBlockWithoutCurlyBrackets() throws ParseError {
 		// Single If-Then condition block without curly brackets
-		statementMatchesConditionBlock("if (true) \"Did you sell a house in 2010?\" hasSoldHouse: boolean");
+		statementMatchesIfThenCondition("if (true) \"Did you sell a house in 2010?\" hasSoldHouse: boolean");
 	}
 	
 	@Test
 	public void testElseBlock() throws ParseError {
 		// Single If-Then-Else condition block with Question as body
-		statementMatchesConditionBlock("if (true) { \"Age?\" age: integer } else { \"Gender?\" gender: string }");
+		statementMatchesIfThenElseCondition("if (true) { \"Age?\" age: integer } else { \"Gender?\" gender: string }");
 	}
 	
 	@Test
 	public void testElseBlockWithoutCurlyBrackets() throws ParseError {
 		// Single If-Then-Else condition block without curly brackets
-		statementMatchesConditionBlock("if (true) \"Age?\" age: integer else \"Gender?\" gender: string");
+		statementMatchesIfThenElseCondition("if (true) \"Age?\" age: integer else \"Gender?\" gender: string");
 	}
 	
 	@Test
 	public void testIfBlockWithMultipleBodyLines() throws ParseError {
 		// Single If-Then condition block with multiple lines as body
-		statementMatchesConditionBlock("if (true) { \"Age?\" age: integer \n \"Name?\" name: string }");
+		statementMatchesIfThenCondition("if (true) { \"Age?\" age: integer \n \"Name?\" name: string }");
 	}
 	
 	@Test
 	public void testInnerIfConditionBlocks() throws ParseError {
 		// Single If-Then condition block with inner If-Then condition block
-		statementMatchesConditionBlock("if (true) { if (wantsToBuyHouse) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean } }");
+		statementMatchesIfThenCondition("if (true) { if (wantsToBuyHouse) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean } }");
 	}
 }
