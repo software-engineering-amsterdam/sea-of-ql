947d4c3526ad0b1092bac3116096009122691b14
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2013-02-18 01:55:10 +0100

    commit of uncommitted changes (minor)

--- ruudvanderweijde/src/org/uva/sea/ql/visitor/renderer/FormRenderer.java
+++ ruudvanderweijde/src/org/uva/sea/ql/visitor/renderer/FormRenderer.java
@@ -25,7 +25,8 @@ import org.uva.sea.ql.visitor.IFormVisitor;
 import org.uva.sea.ql.visitor.valueCheck.ValueMapper;
 
 public class FormRenderer implements IFormVisitor {
-	private STGroup formTemplate = new STGroupFile(System.getProperty("user.dir") + "/files/templates/page.stg", '$', '$');
+	private STGroup formTemplate = new STGroupFile(System.getProperty("user.dir")
+			+ "/files/templates/page.stg", '$', '$');
 	private String formContent = "";
 	private String scriptFunctions = "";
 	private String scriptCommands = "";
@@ -51,11 +52,11 @@ public class FormRenderer implements IFormVisitor {
 
 	@Override
 	public void visit(IfThen ifThen) {
-		final String id = getUniqueString(); 
-		
+		final String id = getUniqueString();
+
 		Expr expr = ifThen.getCondition();
 		addConditionToFormContent(expr, id);
-		
+
 		openBlock("if_" + id);
 		for (Statement stmt : ifThen.getIfBlock()) {
 			stmt.accept(this);
@@ -65,11 +66,11 @@ public class FormRenderer implements IFormVisitor {
 
 	@Override
 	public void visit(IfThenElse ifThenElse) {
-		final String id = getUniqueString(); 
-		
+		final String id = getUniqueString();
+
 		Expr expr = ifThenElse.getCondition();
 		addConditionToFormContent(expr, id);
-		
+
 		openBlock("if_" + id);
 		for (Statement stmt : ifThenElse.getIfBlock()) {
 			stmt.accept(this);
@@ -86,34 +87,33 @@ public class FormRenderer implements IFormVisitor {
 	public void visit(Form form) {
 
 		ST qlPage = formTemplate.getInstanceOf("page");
-			
+
 		for (Statement stmt : form.getStatements()) {
 			stmt.accept(this);
 		}
-		
+
 		qlPage.add("scriptFunctions", getScriptFunctions());
 		qlPage.add("scriptCommands", getScriptCommands());
 		qlPage.add("formContent", getFormContent());
-		
+
 		// render form
 		System.out.print(qlPage.render());
 		// TODO REMOVE THIS DEBUG FILE ABOVE
 		writeOutputToFile(qlPage.render(), "form.html");
 	}
-	
+
 	private void addQuestionToFormContent(Question question, Boolean isReadOnly) {
 		ST qlQuestion = formTemplate.getInstanceOf(question.getType().toString());
 		qlQuestion.add("id", question.getId().getName());
 		qlQuestion.add("label", question.getLabel());
 		qlQuestion.add("readOnly", isReadOnly);
-		
+
 		setFormContent(getFormContent() + qlQuestion.render());
 	}
-	
+
 	private void writeOutputToFile(String output, String fileName) {
 		try {
-			String file_name = System.getProperty("user.dir")
-					+ "/files/output/" + fileName;
+			String file_name = System.getProperty("user.dir") + "/files/output/" + fileName;
 			FileWriter file = new FileWriter(file_name);
 			BufferedWriter out = new BufferedWriter(file);
 			out.write(output);
@@ -122,43 +122,43 @@ public class FormRenderer implements IFormVisitor {
 			System.out.println(e.getMessage());
 		}
 	}
-	
+
 	private void openBlock(String id) {
 		ST qlOpenBlock = formTemplate.getInstanceOf("OpenBlock");
 		qlOpenBlock.add("id", id);
-		addFormContent(qlOpenBlock.render());	
+		addFormContent(qlOpenBlock.render());
 	}
-	
+
 	private void closeBlock() {
 		addFormContent(formTemplate.getInstanceOf("CloseBlock").render());
 	}
 
 	private void addComputationToFormContent(Expr expr, Ident questionId, String uniqueId) {
 		String strCondition = getExpressionStringByExpression(expr, uniqueId);
-		
+
 		ST qlCondition = formTemplate.getInstanceOf("Computation");
 		qlCondition.add("id", questionId.getName());
 		qlCondition.add("uuid", uniqueId);
 		qlCondition.add("condition", strCondition);
-		
+
 		addScriptFunction(qlCondition.render());
 	}
-	
+
 	private void addConditionToFormContent(Expr expr, String uniqueId) {
 		String strCondition = getExpressionStringByExpression(expr, uniqueId);
-		
+
 		ST qlCondition = formTemplate.getInstanceOf("Condition");
 		qlCondition.add("id", uniqueId);
 		qlCondition.add("condition", strCondition);
-		
-		addScriptFunction(qlCondition.render());	
+
+		addScriptFunction(qlCondition.render());
 	}
-	
+
 	private String getExpressionStringByExpression(Expr expr, String uniqueId) {
 		List<Ident> idents = new LinkedList<Ident>();
 		ExpressionRenderer exprRenderer = new ExpressionRenderer(idents);
 		String strCondition = expr.accept(exprRenderer);
-		for(Ident ident : exprRenderer.getIdents()) {
+		for (Ident ident : exprRenderer.getIdents()) {
 			ST qlBindIdToFunction = formTemplate.getInstanceOf("BindIdToFunction");
 			qlBindIdToFunction.add("id", ident.getName());
 			qlBindIdToFunction.add("function", uniqueId);
@@ -166,11 +166,11 @@ public class FormRenderer implements IFormVisitor {
 		}
 		return strCondition;
 	}
-	
+
 	private void addFormContent(String html) {
 		setFormContent(getFormContent() + html);
 	}
-	
+
 	public String getFormContent() {
 		return formContent;
 	}
@@ -178,7 +178,7 @@ public class FormRenderer implements IFormVisitor {
 	public void setFormContent(String formContent) {
 		this.formContent = formContent;
 	}
-	
+
 	public List<Message> getErrors() {
 		return errors;
 	}
@@ -190,7 +190,7 @@ public class FormRenderer implements IFormVisitor {
 	private void addScriptCommand(String html) {
 		setScriptCommands(getScriptCommands() + "\n" + html);
 	}
-	
+
 	public String getScriptCommands() {
 		return scriptCommands;
 	}
@@ -198,11 +198,11 @@ public class FormRenderer implements IFormVisitor {
 	public void setScriptCommands(String scriptCommands) {
 		this.scriptCommands = scriptCommands;
 	}
-	
+
 	private void addScriptFunction(String html) {
 		setScriptFunctions(getScriptFunctions() + "\n" + html);
 	}
-	
+
 	public String getScriptFunctions() {
 		return scriptFunctions;
 	}
@@ -210,7 +210,7 @@ public class FormRenderer implements IFormVisitor {
 	public void setScriptFunctions(String scriptFunction) {
 		this.scriptFunctions = scriptFunction;
 	}
-	
+
 	private String getUniqueString() {
 		return UUID.randomUUID().toString().replaceAll("-", "");
 	}
