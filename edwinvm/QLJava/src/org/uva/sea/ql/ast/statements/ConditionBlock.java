53e09f978db951640a5f8be0dcf3b58be18ba14f
Edwin van Manen <edwin.vanmanen@student.uva.nl>
2013-02-18 17:27:00 +0100

    Changed ConditionBlock to be a super class

--- edwinvm/QLJava/src/org/uva/sea/ql/ast/statements/ConditionBlock.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/ast/statements/ConditionBlock.java
@@ -2,37 +2,15 @@ package org.uva.sea.ql.ast.statements;
 
 import org.uva.sea.ql.ast.Expr;
 import org.uva.sea.ql.ast.FormStatement;
-import org.uva.sea.ql.ast.visitors.statementchecker.Visitor;
 
-public class ConditionBlock extends FormStatement {
-	private final Expr _expr;
-	private final FormStatement _ifBody;
-	private final FormStatement _elseBody;
-
-	public ConditionBlock(Expr condition, FormStatement ifBody) {
-		this(condition, ifBody, null);
+public abstract class ConditionBlock extends FormStatement {
+	private final Expr _condition;
+	
+	public ConditionBlock(Expr condition) {
+		_condition = condition;
 	}
 	
-	public ConditionBlock(Expr expr, FormStatement ifBody, FormStatement elseBody) {
-		_expr = expr;
-		_ifBody = ifBody;
-		_elseBody = elseBody;
-	}
-	
-	public Expr getExpr() {
-		return _expr;
-	}
-	
-	public FormStatement getIfBody() {
-		return _ifBody;
-	}
-	
-	public FormStatement getElseBody() {
-		return _elseBody;
-	}
-
-	@Override
-	public void accept(Visitor visitor) {
-		visitor.visit(this);
+	public Expr getCondition() {
+		return _condition;
 	}
 }
