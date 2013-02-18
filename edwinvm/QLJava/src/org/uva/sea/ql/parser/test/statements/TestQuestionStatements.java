7a03a352c3b5bce879bf87da0317fe71c0759df7
Edwin van Manen <edwin.vanmanen@student.uva.nl>
2013-02-18 15:02:24 +0100

    Added unit test cases for more advanced computed questions statement type checking

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/statements/TestQuestionStatements.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/statements/TestQuestionStatements.java
@@ -12,6 +12,27 @@ public class TestQuestionStatements extends StatementTypeChecker {
 	}
 	
 	@Test
+	public void testComputedQuestions() throws ParseError {
+		isAValidStatement(
+			"\"Price the house was sold for:\" sellingPrice: integer" +
+			"\"Price the house was bought for:\" privateDebt: integer" + 
+			"\"Did you sell a house in 2010?\" valueResidue = sellingPrice - privateDebt"
+		);
+		
+		isAValidStatement(
+			"\"Price the house was sold for:\" sellingPrice: integer" + 
+			"\"Price the house was bought for:\" privateDebt: integer" + 
+			"\"Your profit percentage: \" percentageResidue = sellingPrice * 100 / privateDebt"
+		);
+		
+		isAValidStatement(
+			"\"What is your first name?\" firstName: string" +
+			"\"What is your last name?\" lastName: string" +
+			"\"Full name:\" fullName = firstName + lastName"
+		);
+	}
+	
+	@Test
 	public void testQuestionTypes() throws ParseError {
 		isOfTypeBoolean("\"Did you sell a house in 2010?\" hasSoldHouse: boolean");
 		isOfTypeNumeric("\"Price the house was sold for:\" sellingPrice: money");
@@ -21,9 +42,23 @@ public class TestQuestionStatements extends StatementTypeChecker {
 	}
 	
 	@Test
-	public void testComputedQuestions() throws ParseError {
-		isAValidStatement("\"Did you sell a house in 2010?\" valueResidue = sellingPrice - privateDebt");
-		isAValidStatement("\"Your profit percentage: \" percentageResidue = sellingPrice * 100 / privateDebt");
-		isAValidStatement("\"Full name:\" fullName = firstName + lastName");
+	public void testComputedQuestionsTypes() throws ParseError {
+		isOfTypeNumeric(
+			"\"Price the house was sold for:\" sellingPrice: integer" +
+			"\"Price the house was bought for:\" privateDebt: integer" + 
+			"\"Did you sell a house in 2010?\" valueResidue = sellingPrice - privateDebt"
+		);
+		
+		isOfTypeNumeric(
+			"\"Price the house was sold for:\" sellingPrice: integer" + 
+			"\"Price the house was bought for:\" privateDebt: integer" + 
+			"\"Your profit percentage: \" percentageResidue = sellingPrice * 100 / privateDebt"
+		);
+		
+		isOfTypeString(
+			"\"What is your first name?\" firstName: string" +
+			"\"What is your last name?\" lastName: string" +
+			"\"Full name:\" fullName = firstName + lastName"
+		);
 	}
 }
