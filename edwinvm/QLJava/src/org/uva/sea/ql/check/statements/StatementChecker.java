4222becdc97ed017f017276ebb10ceeb016fc2b7
Edwin van Manen <edwin.vanmanen@student.uva.nl>
2013-02-18 13:11:05 +0100

    Implemented functionality for StatementChecker

--- edwinvm/QLJava/src/org/uva/sea/ql/check/statements/StatementChecker.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/check/statements/StatementChecker.java
@@ -1,34 +1,71 @@
 package org.uva.sea.ql.check.statements;
 
+import org.uva.sea.ql.ast.Expr;
 import org.uva.sea.ql.ast.FormStatement;
+import org.uva.sea.ql.ast.Type;
+import org.uva.sea.ql.ast.expressions.literal.Ident;
 import org.uva.sea.ql.ast.statements.ConditionBlock;
+import org.uva.sea.ql.ast.statements.Question;
 import org.uva.sea.ql.ast.statements.questions.AnswerableQuestion;
 import org.uva.sea.ql.ast.statements.questions.ComputedQuestion;
 import org.uva.sea.ql.ast.visitors.statementchecker.Visitor;
+import org.uva.sea.ql.check.expressions.TypeChecker;
+import org.uva.sea.ql.parser.ErrorMessages;
+import org.uva.sea.ql.parser.SupportedTypes;
 
 public class StatementChecker implements Visitor {
 	
-	public static void check(FormStatement statement) {
-		StatementChecker statementChecker = new StatementChecker();
+	private final SupportedTypes _supportedTypes;
+	private final ErrorMessages _errorMessages;
+	
+	private StatementChecker(SupportedTypes supportedTypes, ErrorMessages messages) {
+		_supportedTypes = supportedTypes;
+		_errorMessages = messages;
+	}
+	
+	public static void check(FormStatement statement, SupportedTypes supportedTypes, ErrorMessages errors) {
+		StatementChecker statementChecker = new StatementChecker(supportedTypes, errors);
 		statement.accept(statementChecker);
 	}
 
 	@Override
 	public void visit(ConditionBlock statement) {
-		// TODO Auto-generated method stub
-		
+		checkCondition(statement);
+		statement.getIfBody().accept(this);
 	}
 
 	@Override
 	public void visit(AnswerableQuestion statement) {
-		// TODO Auto-generated method stub
-		
+		checkName(statement, statement.getType());
 	}
 
 	@Override
 	public void visit(ComputedQuestion statement) {
-		// TODO Auto-generated method stub
-		
+		checkName(statement, statement.getExpression().typeOf(_supportedTypes));
+		checkExpr(statement.getExpression());
+	}
+	
+	private void checkCondition(ConditionBlock statement) {
+		checkExpr(statement.getExpr());
+	}
+
+	private void checkExpr(Expr expr) {
+		// Run expression through TypeChecker
+		TypeChecker.check(expr, _supportedTypes, _errorMessages);
+	}
+	
+	private void checkName(Question statement, Type type) {
+		Ident questionVariable = statement.getVariable();
+		storeQuestionVariable(questionVariable, type);
+		if (!type.isCompatibleTo(questionVariable.typeOf(_supportedTypes))) {
+			_errorMessages.add("Invalid type for identifier " + questionVariable.getValue().toString());
+		}
+	}
+	
+	private void storeQuestionVariable(Ident questionVariable, Type type) {
+		if (!_supportedTypes.contains(questionVariable)) {
+			_supportedTypes.add(questionVariable.toString(), type);
+		}
 	}
 	
 }
