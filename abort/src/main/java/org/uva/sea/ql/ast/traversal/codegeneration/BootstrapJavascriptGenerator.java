67ed5abfacaa12270df005651a695244e9ad3104
abort <abort@digitalise.net>
2013-02-18 04:44:32 +0100

    some more documentation

--- abort/src/main/java/org/uva/sea/ql/ast/traversal/codegeneration/BootstrapJavascriptGenerator.java
+++ abort/src/main/java/org/uva/sea/ql/ast/traversal/codegeneration/BootstrapJavascriptGenerator.java
@@ -5,24 +5,50 @@ import java.util.*;
 import org.stringtemplate.v4.*;
 import org.uva.sea.ql.ast.types.Ident;
 
+/**
+ * Bootstrap Javascript Generator.
+ * 
+ * @author J. Dijkstra
+ */
 class BootstrapJavascriptGenerator {
+	/**
+	 * Template File Name.
+	 */
 	private static final String TEMPLATE_FILE_NAME = "bootstrap_js.stg";
+
+	/**
+	 * Template delimiter character.
+	 */
 	private static final char TEMPLATE_DELIMITER = '$';
 	
+	/**
+	 * Template group file to use. 
+	 */
 	private final STGroupFile javascriptTemplateGroup;
 	
+	/**
+	 * Init code on top of the javascript.
+	 */
 	private final StringBuffer initialCode = new StringBuffer();
 
-	// Amount of references to a specific identifier
+	/**
+	 * Holds amount of references to a specific identifier for code generation.
+	 */
 	private final Map<Ident, Integer> references = new HashMap<Ident, Integer>();
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param templatesPath path to retrieve the code generation templates (stringtemplates) from.
+	 * @param serverBaseURL base URL for the server to use in the front-end code.
+	 */
 	public BootstrapJavascriptGenerator(final String templatesPath, final String serverBaseURL) {
 		final String javascriptTemplateFilePath = String.format("%s/%s", templatesPath, TEMPLATE_FILE_NAME);
 		javascriptTemplateGroup = new STGroupFile(javascriptTemplateFilePath, TEMPLATE_DELIMITER, TEMPLATE_DELIMITER);
 	
 		initServerPort(serverBaseURL);
 	}
-	
+
 	private void initServerPort(final String serverBaseURL) {
 		final ST baseURLDeclarationTemplate = javascriptTemplateGroup.getInstanceOf("server_base_url_declaration");
 		baseURLDeclarationTemplate.add("url", serverBaseURL);
@@ -30,14 +56,30 @@ class BootstrapJavascriptGenerator {
 		initialCode.append(baseURLDeclarationTemplate.render());
 	}
 	
+	/**
+	 * Init reference count to an ident.
+	 * 
+	 * @param id ident
+	 */
 	public void initIdentReference(final Ident id) {
 		references.put(id, 0);
 	}
 	
+	/**
+	 * Increase reference count to an ident.
+	 * 
+	 * @param id ident
+	 */
 	public void increaseIdentReferenceCount(final Ident id) {
 		references.put(id, references.get(id) + 1);
 	}
 	
+	/**
+	 * Declare a javascript function.
+	 * 
+	 * @param id ident
+	 * @param expression expression template
+	 */	
 	public void declareFunction(final Ident id, final ST expression) {
 		declareFunction(id.getName(), expression);
 		
@@ -45,6 +87,12 @@ class BootstrapJavascriptGenerator {
 		initIdentReference(id);
 	}
 	
+	/**
+	 * Declare a javascript function.
+	 * 
+	 * @param id id of the variable
+	 * @param expression expression template
+	 */
 	public void declareFunction(final String id, final ST expression) {
 		// Store the javascript function in the initialization code
 		final ST functionTemplate = javascriptTemplateGroup.getInstanceOf("generate_map_function");
@@ -54,6 +102,10 @@ class BootstrapJavascriptGenerator {
 		initialCode.append(functionTemplate.render());
 	}
 	
+	/**
+	 * Render javascript code.
+	 * @return javascript code in a string
+	 */
 	public String render() {
 		final StringBuffer documentReadyBuffer = new StringBuffer();
 		final StringBuffer javascriptBuffer = new StringBuffer();
