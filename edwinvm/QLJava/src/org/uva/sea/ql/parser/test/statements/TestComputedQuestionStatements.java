b2049d73493a3ab037593ad2b5b41595bcdfa0a1
Edwin van Manen <edwin.vanmanen@student.uva.nl>
2013-02-19 16:33:22 +0100

    Refactored ComputedQuestion unit tests to check over multiple statements

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/statements/TestComputedQuestionStatements.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/statements/TestComputedQuestionStatements.java
@@ -6,23 +6,13 @@ import org.uva.sea.ql.parser.ParseError;
 public class TestComputedQuestionStatements extends StatementTypeChecker {
 	@Test
 	public void testComputedQuestions() throws ParseError {
-		isAValidStatement(
-			"\"Price the house was sold for:\" sellingPrice: integer" +
-			"\"Price the house was bought for:\" privateDebt: integer" + 
-			"\"Did you sell a house in 2010?\" valueResidue = sellingPrice - privateDebt"
-		);
+		// First we need AnswerableQuestions to store the identifiers used in the ComputedQuestions
+		isAValidStatement("\"Price the house was sold for:\" sellingPrice: integer");
+		isAValidStatement("\"Price the house was bought for:\" privateDebt: integer");
 		
-		isAValidStatement(
-			"\"Price the house was sold for:\" sellingPrice: integer" + 
-			"\"Price the house was bought for:\" privateDebt: integer" + 
-			"\"Your profit percentage: \" percentageResidue = sellingPrice * 100 / privateDebt"
-		);
-		
-		isAValidStatement(
-			"\"What is your first name?\" firstName: string" +
-			"\"What is your last name?\" lastName: string" +
-			"\"Full name:\" fullName = firstName + lastName"
-		);
+		// Check ComputedQuestions based on stored identifiers
+		isAValidStatement("\"Did you sell a house in 2010?\" valueResidue = sellingPrice - privateDebt");
+		isAValidStatement("\"Your profit percentage: \" percentageResidue = sellingPrice * 100 / privateDebt");
 	}
 	
 	@Test
