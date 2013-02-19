2b8e7e5051c8f9330bd40c01091916a6bbf7bb88
Edwin van Manen <edwin.vanmanen@student.uva.nl>
2013-02-19 18:03:04 +0100

    Beginning of new unit test for Conditional statement checking

---
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/statements/TestIfThenStatements.java
@@ -0,0 +1,14 @@
+package org.uva.sea.ql.parser.test.statements;
+
+import org.junit.Test;
+import org.uva.sea.ql.parser.ParseError;
+
+public class TestIfThenStatements extends ConditionTypeChecker {
+	@Test
+	public void testIfThenElse() throws ParseError {
+		// Single If-Then condition block with Question as body
+		isAValidStatement("if (true) { \"Did you want to buy a house?\" wantsToBuyHouse: boolean }");
+		isAValidStatement("if (wantsToBuyHouse) { \"What is your name?\" age: integer }");
+		isAValidStatement("if (wantsToBuyHouse && age >= 18) { \"Did you sell a house in 2010?\" hasSoldHouse: boolean }");
+	}
+}
