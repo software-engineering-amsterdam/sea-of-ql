b145ef726de89b85357b74d42a1503950601da93
Edwin van Manen <edwin.vanmanen@student.uva.nl>
2013-02-22 17:18:29 +0100

    Moved composite class BinaryRelExpr to subpackage

--- edwinvm/QLJava/src/org/uva/sea/ql/ast/expressions/binary/relational/BinaryRelationalExpr.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/ast/expressions/binary/relational/BinaryRelationalExpr.java
@@ -1,4 +1,4 @@
-package org.uva.sea.ql.ast.expressions.binary;
+package org.uva.sea.ql.ast.expressions.binary.relational;
 
 import org.uva.sea.ql.ast.expressions.BinaryExpr;
 import org.uva.sea.ql.ast.expressions.Expr;
