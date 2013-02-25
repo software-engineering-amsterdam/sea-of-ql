7474fa6e29a5f69f7792de5f03d75c0689c6b657
Edwin van Manen <edwin.vanmanen@student.uva.nl>
2013-02-25 13:41:35 +0100

    Fixed Evaluator visitor

--- edwinvm/QLJava/src/org/uva/sea/ql/check/values/Evaluator.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/check/values/Evaluator.java
@@ -1,6 +1,7 @@
 package org.uva.sea.ql.check.values;
 
 import org.uva.sea.ql.ast.expressions.Expression;
+import org.uva.sea.ql.ast.expressions.Identifier;
 import org.uva.sea.ql.ast.expressions.binary.BinaryExpression;
 import org.uva.sea.ql.ast.expressions.binary.arithmetic.Addition;
 import org.uva.sea.ql.ast.expressions.binary.arithmetic.Division;
@@ -14,15 +15,14 @@ import org.uva.sea.ql.ast.expressions.binary.relational.GreaterThanOrEqualToExpr
 import org.uva.sea.ql.ast.expressions.binary.relational.LessThanExpression;
 import org.uva.sea.ql.ast.expressions.binary.relational.LessThanOrEqualToExpression;
 import org.uva.sea.ql.ast.expressions.binary.relational.NotEqualToExpression;
-import org.uva.sea.ql.ast.expressions.literal.Bool;
-import org.uva.sea.ql.ast.expressions.literal.Identifier;
-import org.uva.sea.ql.ast.expressions.literal.Int;
-import org.uva.sea.ql.ast.expressions.literal.Money;
-import org.uva.sea.ql.ast.expressions.literal.Str;
 import org.uva.sea.ql.ast.expressions.unary.NegationalExpression;
 import org.uva.sea.ql.ast.expressions.unary.NegativeExpression;
 import org.uva.sea.ql.ast.expressions.unary.PositiveExpression;
+import org.uva.sea.ql.ast.values.Bool;
+import org.uva.sea.ql.ast.values.Int;
+import org.uva.sea.ql.ast.values.Money;
 import org.uva.sea.ql.ast.values.NullValue;
+import org.uva.sea.ql.ast.values.Str;
 import org.uva.sea.ql.ast.values.Value;
 import org.uva.sea.ql.ast.visitors.typechecker.Visitor;
 import org.uva.sea.ql.parser.ValueEnvironment;
@@ -35,8 +35,11 @@ public class Evaluator implements Visitor<Value> {
 		//_valueEnvironment = (ValueEnvironment) Collections.unmodifiableMap(valueEnvironment.getEnvironment());
 		_valueEnvironment = valueEnvironment;
 	}
-	
-	public void eval(Expression expression) { expression.accept(this); }
+
+	public static Value eval(Expression expression, ValueEnvironment valueEnvironment) {
+		Evaluator evaluator = new Evaluator(valueEnvironment);
+		return expression.accept(evaluator);
+	}
 
 	@Override
 	public Value visit(Addition expression)       { return getLeftHandSide(expression).add(getRightHandSide(expression)); }
@@ -77,24 +80,27 @@ public class Evaluator implements Visitor<Value> {
 	public Value visit(PositiveExpression expression)               { return new NullValue(); }
 	
 	@Override
-	public Value visit(Bool expression) { 
+	public Value visit(Bool value) { 
 		return new NullValue(); 
 	}
 
 	@Override
-	public Value visit(Int expression) {
+	public Value visit(Int value) {
+		return value;
+	}
+
+	@Override
+	public Value visit(Money value) {
 		return null;
 	}
 
 	@Override
-	public Value visit(Money expression) {
+	public Value visit(Str value) {
 		return null;
 	}
-
+	
 	@Override
-	public Value visit(Str expression) {
-		return null;
-	}
+	public Value visit(NullValue astNode) { return new NullValue(); }
 	
 	private Value getLeftHandSide(BinaryExpression expression)  { return expression.getLeftHandSide().accept(this); }
 	private Value getRightHandSide(BinaryExpression expression) { return expression.getRightHandSide().accept(this); }
