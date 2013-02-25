115f74bf3415e4de72e1de972077cdc6bc7fe317
Edwin van Manen <edwin.vanmanen@student.uva.nl>
2013-02-25 17:17:16 +0100

    Added all other logical expression value checking unit test cases

--- edwinvm/QLJava/src/org/uva/sea/ql/parser/test/values/TestBinaryLogicalExpressionValues.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/parser/test/values/TestBinaryLogicalExpressionValues.java
@@ -6,11 +6,15 @@ import org.uva.sea.ql.parser.errors.ParseError;
 public class TestBinaryLogicalExpressionValues extends ExpressionValueChecker {
 	@Test
 	public void testAnds() throws ParseError {
-		expressionMatchesValue("true && true", "true");
+		expressionMatchesValue("true && true",   "true");
+		expressionMatchesValue("true && false",  "false");
+		expressionMatchesValue("false && false", "false");
 	}
 	
 	@Test
 	public void testOrs() throws ParseError {
-		expressionMatchesValue("true || true", "true");
+		expressionMatchesValue("true || true",   "true");
+		expressionMatchesValue("true || false",  "true");
+		expressionMatchesValue("false || false", "false");
 	}
 }
