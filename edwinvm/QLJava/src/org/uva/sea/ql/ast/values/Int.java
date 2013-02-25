8cf52adf4b484ceb2cebb205d7781b7e952a0b0e
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-25 01:17:55 +0100

    Implementation of computation methods in Int Value class

--- edwinvm/QLJava/src/org/uva/sea/ql/ast/values/Int.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/ast/values/Int.java
@@ -1,6 +1,7 @@
 package org.uva.sea.ql.ast.values;
 
 public class Int extends Value {
+	
 	private final Integer _value;
 	
 	public Int(Integer value) {
@@ -10,4 +11,25 @@ public class Int extends Value {
 	public Integer getValue() {
 		return _value;
 	}
+	
+	@Override
+	public Value add(Value value) { return value.addInt(this); }
+	@Override
+	public Value sub(Value value) { return value.subInt(this); }
+	@Override
+	public Value div(Value value) { return value.divInt(this); }
+	@Override
+	public Value mul(Value value) { return value.mulInt(this); }
+	
+	// NB: below the arguments are reversed because of double dispatch.
+
+	@Override
+	protected Value addInt(Int value) { return new Int(value.getValue() + getValue()); }
+	@Override
+	protected Value subInt(Int value) { return new Int(value.getValue() - getValue()); }
+	@Override
+	protected Value mulInt(Int value) { return new Int(value.getValue() * getValue()); }
+	@Override
+	protected Value divInt(Int value) { return new Int(value.getValue() / getValue()); }
+
 }
