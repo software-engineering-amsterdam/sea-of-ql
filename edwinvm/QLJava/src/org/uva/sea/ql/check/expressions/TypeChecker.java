9cd726b18c0107d50f3bf8b8492a51e699159cb0
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-28 21:35:14 +0100

    Made new super class for TypeChecker visitor helper methods

--- edwinvm/QLJava/src/org/uva/sea/ql/check/expressions/TypeChecker.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/check/expressions/TypeChecker.java
@@ -21,8 +21,6 @@ import org.uva.sea.ql.ast.expressions.binary.relational.NotEqualToExpression;
 import org.uva.sea.ql.ast.expressions.unary.NegationalExpression;
 import org.uva.sea.ql.ast.expressions.unary.NegativeExpression;
 import org.uva.sea.ql.ast.expressions.unary.PositiveExpression;
-import org.uva.sea.ql.ast.expressions.unary.UnaryExpression;
-import org.uva.sea.ql.ast.types.Type;
 import org.uva.sea.ql.ast.values.Bool;
 import org.uva.sea.ql.ast.values.Int;
 import org.uva.sea.ql.ast.values.Money;
@@ -31,16 +29,11 @@ import org.uva.sea.ql.ast.values.Str;
 import org.uva.sea.ql.ast.visitors.typechecker.Visitor;
 import org.uva.sea.ql.parser.TypeEnvironment;
 import org.uva.sea.ql.parser.errors.ErrorMessages;
-import org.uva.sea.ql.parser.errors.Message;
 
-public class TypeChecker implements Visitor<Boolean> {
-	
-	private final TypeEnvironment _typeEnvironment;
-	private final ErrorMessages _errorMessages;
+public class TypeChecker extends ExpressionChecker implements Visitor<Boolean> {
 	
 	private TypeChecker(TypeEnvironment typeEnvironment, ErrorMessages messages) {
-		_typeEnvironment = typeEnvironment;
-		_errorMessages = messages;
+		super(typeEnvironment, messages);
 	}
 	
 	public static boolean check(Expression expression, TypeEnvironment typeEnvironment, ErrorMessages errors) {
@@ -107,25 +100,6 @@ public class TypeChecker implements Visitor<Boolean> {
 		return isAValidExpression(expression) && isCompatibleToBool(expression, binarySymbol);
 	}
 	
-	private Boolean checkUnaryArithmeticExpression(UnaryExpression expression, String binarySymbol) {
-		return isCompatibleToNumeric(expression, binarySymbol);
-	}
-	
-	private Boolean checkUnaryLogicalExpression(UnaryExpression expression, String binarySymbol) {
-		return isCompatibleToBool(expression, binarySymbol);
-	}
-	
-	private Boolean checkLiteralExpression(Expression expression, String className) {
-		return isCompatibleTo(expression, className);
-	}
-	
-	private Expression getLeftHandSide(BinaryExpression expression)  { return expression.getLeftHandSide();  }
-	private Expression getRightHandSide(BinaryExpression expression) { return expression.getRightHandSide(); }
-	
-	private Type getTypeFor(Expression expression)                   { return expression.typeOf(_typeEnvironment); }
-	private Type getLeftHandSideType(BinaryExpression expression)    { return getLeftHandSide(expression).typeOf(_typeEnvironment);  }
-	private Type getRightHandSideType(BinaryExpression expression)   { return getRightHandSide(expression).typeOf(_typeEnvironment); }
-	
 	private boolean isAValidExpression(BinaryExpression expression) {
 		Expression leftHandSide  = getLeftHandSide(expression);
 		Expression rightHandSide = getRightHandSide(expression);
@@ -134,47 +108,6 @@ public class TypeChecker implements Visitor<Boolean> {
 		boolean checkRightHandSide = rightHandSide.accept(this);
 		
 		return checkLeftHandSide && checkRightHandSide;
-	}
-	
-	private boolean isCompatibleToNumeric(BinaryExpression expression, String binarySymbol) {
-		boolean isCompatible = getLeftHandSideType(expression).isCompatibleToNumeric() && getRightHandSideType(expression).isCompatibleToNumeric();
-		return checkTypes(expression, binarySymbol, isCompatible);
-	}
-	
-	private boolean isCompatibleToNumeric(UnaryExpression expression, String binarySymbol) {
-		boolean isCompatible = getTypeFor(expression).isCompatibleToNumeric();
-		return checkTypes(expression, binarySymbol, isCompatible);
-	}
-	
-	private boolean isCompatibleToBool(BinaryExpression expression, String binarySymbol) {
-		boolean isCompatible = getLeftHandSideType(expression).isCompatibleToBool() && getRightHandSideType(expression).isCompatibleToBool();
-		return checkTypes(expression, binarySymbol, isCompatible);
-	}
-	
-	private boolean isCompatibleToBool(UnaryExpression expression, String binarySymbol) {
-		boolean isCompatible = getTypeFor(expression).isCompatibleToBool();
-		return checkTypes(expression, binarySymbol, isCompatible);
-	}
-	
-	private boolean isCompatibleTo(BinaryExpression expression, String binarySymbol) {
-		boolean isCompatible = getLeftHandSideType(expression).isCompatibleTo(getRightHandSideType(expression));
-		return checkTypes(expression, binarySymbol, isCompatible);
-	}
-	
-	private boolean isCompatibleTo(Expression expression, String binarySymbol) {
-		boolean isCompatible = getTypeFor(expression).isCompatibleTo(getTypeFor(expression));
-		return checkTypes(expression, binarySymbol, isCompatible);
-	}
-	
-	private boolean checkTypes(Expression expression, String binarySymbol, boolean isCompatible) {
-		if (!isCompatible) {
-			addError(expression, "invalid type for " + binarySymbol);
-		}
-		return isCompatible;
-	}
-	
-	private void addError(Expression expression, String errorMessage) {
-		_errorMessages.add(new Message("Type error for expr " + expression + ": " + errorMessage));
 	}
 	
 }
