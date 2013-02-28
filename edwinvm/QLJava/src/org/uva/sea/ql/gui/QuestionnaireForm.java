7aa0e6e3b2a4411fbcf0bbd1b4bc6ba1381d2bd1
Edwin <edwin.vanmanen@student.uva.nl>
2013-02-28 02:15:24 +0100

    Added support for closing GUI on escape key

--- edwinvm/QLJava/src/org/uva/sea/ql/gui/QuestionnaireForm.java
+++ edwinvm/QLJava/src/org/uva/sea/ql/gui/QuestionnaireForm.java
@@ -1,8 +1,13 @@
 package org.uva.sea.ql.gui;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.KeyStroke;
 
 import org.uva.sea.ql.ast.expressions.Identifier;
 import org.uva.sea.ql.ast.forms.Form;
@@ -42,8 +47,8 @@ public class QuestionnaireForm {
 	}
 	
 	
-	private void initializeForm(Identifier identifier) {
-		_questionnaireFrame = new JFrame(identifier.getValue());
+	private void initializeForm(Identifier formName) {
+		_questionnaireFrame = new JFrame(formName.getValue());
 	}
 	
 	private void setFormLayout() {
@@ -51,6 +56,17 @@ public class QuestionnaireForm {
 		_questionnaireFrame.setLayout(new FlowLayout());
 		_questionnaireFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		_questionnaireFrame.setVisible(true);
+		enableCloseOnEscape();
+	}
+
+	private void enableCloseOnEscape() {
+		ActionListener escListener = new ActionListener() {
+	        @Override
+	        public void actionPerformed(ActionEvent e) { _questionnaireFrame.dispose(); }
+	    };
+	    _questionnaireFrame.getRootPane().registerKeyboardAction(escListener,
+            KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW
+        );
 	}
 	
 }
