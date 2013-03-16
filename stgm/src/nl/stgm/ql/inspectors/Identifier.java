6f00688ff2af52e15fd8b0b95e42c5f2774779b7
Martijn Stegeman <martijn@stgm.nl>
2013-03-16 13:26:56 +0100

    damn

---
+++ stgm/src/nl/stgm/ql/inspectors/Identifier.java
@@ -0,0 +1,50 @@
+package nl.stgm.ql.inspectors;
+
+public class Identifier
+{
+	public static enum Type
+	{
+		INT, BOOL, STRING;
+		
+		public static Type parse(String source)
+		{
+			switch(source)
+			{
+				case "int":
+					return INT;
+				case "boolean":
+					return BOOL;
+				case "string":
+					return STRING;
+			}
+			
+			return null;
+		}
+	}
+
+	String name;
+	Type type;
+	boolean computed;
+	
+	public Identifier(String name, String type, boolean isComputed)
+	{
+		this.name = name;
+		this.type = Type.parse(type);
+		this.computed = computed;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public Type getType()
+	{
+		return this.type;
+	}
+	
+	public boolean isComputed()
+	{
+		return computed;
+	}
+}
