9217955e97aee14c6d1a74adfbb949c25e85276d
Martijn Stegeman <martijn@stgm.nl>
2013-04-20 23:35:50 +0200

    extraneous boot code removed

--- stgm/src/nl/stgm/ql/interpreter/Interpreter.java
+++ stgm/src/nl/stgm/ql/interpreter/Interpreter.java
@@ -5,25 +5,10 @@ import nl.stgm.ql.ast.form.Document;
 
 public class Interpreter extends RunnableVisitor
 {
-	private Document astDocument;
-	private InterpreterViewManager manager;
-	private InterpreterController controller;
-	
-	public Interpreter(Document astDocument)
-	{
-		this.astDocument = astDocument;
-		this.controller = new InterpreterController(astDocument);
-	}
-	
-	public void run()
-	{
-		controller.run();
-	}
-
 	public static void main(String[] args)
 	{
 		Document document = parseDocument("elaborate.qldoc");
-		Interpreter app = new Interpreter(document);
+		InterpreterController app = new InterpreterController(document);
 		app.run();
 	}
 }
