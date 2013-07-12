61d3810e02a4f90a92e0e3fd9d9967fd059a9a08
Steven van Ombergen <svombergen@hotmail.com>
2013-07-12 02:15:34 +0200

    removed comments

--- svombergen/QLJava/src/org/uva/sea/ql/parser/antlr/QLRun.java
+++ svombergen/QLJava/src/org/uva/sea/ql/parser/antlr/QLRun.java
@@ -13,28 +13,17 @@ public class QLRun {
 
 	public static void main(String[] args) throws Exception {
 		ANTLRParser parser = new ANTLRParser();
-		//Form f = parser.parseForm("form abc {ding1:\"ding\" boolean if (ding1) { ding2:\"bla2\" boolean}}");
 		
-		//Form form1 = parser.parseForm("form Box1HouseOwning {\n	valueHouse: “Value of house?” int\n	computeValue: “Double value of house:” int(2 * valueHouse)\n		hasSoldHouse: “Did you sell a house in 2010?” boolean\n   hasBoughtHouse: “Did you buy a house in 2010?” boolean\n   hasMaintLoan: “Did you enter a loan for maintenance/reconstruction?”\nboolean\n   if (hasSoldHouse) {\n     sellingPrice: “Price the house was sold for:” money\n     privateDebt: “Private debts for the sold house:” money\n     valueResidue: “Value residue:” money(sellingPrice - privateDebt)\n   }\n}");
-		Form form1 = parser.parseForm(readFile("C:\\Users\\Steven\\Documents\\GitHub\\sea-of-ql\\svombergen\\QLJava\\src\\org\\uva\\sea\\ql\\parser\\test\\toParse.txt"));
+		Form form1 = parser.parseForm(readFile(
+				"C:\\Users\\Steven\\Documents\\GitHub\\sea-of-ql\\svombergen\\QLJava\\src\\org\\uva\\sea\\ql\\parser\\test\\toParse.txt"
+				));
 		CheckFormCorrectnessVisitor correctVisitor = new CheckFormCorrectnessVisitor();
-		form1.accept(correctVisitor);
-		FormRenderer formRenderer1 = new FormRenderer();
-		form1.accept(formRenderer1);
-		System.out.println("form: " + form1);
 		
-		
-		
-//		Expr exp = parser.parse("3 * (4 + 6) / 10");
-//		TypeCheckExpressionVisitor exprVis = new TypeCheckExpressionVisitor();
-//		ValueExpressionVisitor exprVis2 = new ValueExpressionVisitor(null);
-//		exp.accept(exprVis);
-//		System.out.println(exp.accept(exprVis2));
-//		System.out.println(exprVis.returnErrorMsgs());
-//		System.out.println(exprVis.returnWarningMsgs());
-//		System.out.println(exp.calculateValue());
-		
-
+		if (form1.accept(correctVisitor)) {
+			FormRenderer formRenderer1 = new FormRenderer();
+			form1.accept(formRenderer1);
+			System.out.println("form: " + form1);
+		}
 	}
 	
 	public static String readFile(String location) {
